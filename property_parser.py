6f1a287eab22d026762336c8924969dc4edbecda
Darin Douglass <douglassdarin@gmail.com>
2014-09-24 14:48:15 -0400

    Update property_parser.py and create utils file.
    Update the Property class to better conform to Python's PEP8 style
    guide. Also, pulled out the clean_line method into a utility file where
    it is better suited.

--- property_parser.py
+++ property_parser.py
@@ -19,65 +19,64 @@ to_strings: => string
 
 import re
 
-class Property:
-  '''Represents Property found in property files, like those used by valve.'''
-  def __init__(self, name = None, value = ""):
-    self.name = name
-    self.value = value
+import utils
 
-  @staticmethod
-  def parse(file_contents):
-    '''Returns list of Property objects parsed from given text'''
-    open_properties = [Property(None, [])]
-    values = None
-    line_num = 0
-    for line in file_contents:
-      line_num += 1
-      values = open_properties[-1].value
-      freshline = Property.__clean_line__(line)
-      if freshline:
-        if freshline.startswith('"'):
-          line_contents = str.split(freshline, '"')
-          name = line_contents[1]
-          if len(line_contents)>3:
-            value = line_contents[3]
-          else:
-            value = None
-          values.append(Property(name, value))
-        elif freshline.startswith('{'):
-          if not values[-1].value:
-            values[-1].value = []
-          else:
-            raise ValueError("Property cannot have sub-section if it already has an in-line value. Line " + str(line_num) + ".")
-          open_properties.append(values[-1])
-        elif(freshline.startswith('}')):
-          open_properties.pop()
+class Property:
+  '''Represents Property found in property files, like those used by Valve.'''
+    def __init__(self, name = None, value = ""):
+        self.name = name
+        self.value = value
+
+    @staticmethod
+    def parse(file_contents):
+        '''Returns list of Property objects parsed from given text'''
+        open_properties = [Property(None, [])]
+        values = None
+
+        for line_num, line in enumerate(file_contents):
+            values = open_properties[-1].value
+            freshline = utils.clean_line(line)
+            if freshline:
+                if freshline.startswith('"'):
+                    line_contents = freshline.split('"')
+                    name = line_contents[1]
+                    
+                    try:
+                        value = line_contents[3]
+                    except IndexError:
+                        value = None
+                        
+                    values.append(Property(name, value))
+                elif freshline.startswith('{'):
+                    if values[-1].value:
+                        raise ValueError("Property cannot have sub-section if it already has an in-line value. Line " + str(line_num) + ".")
+                    
+                    values[-1].value = []
+                    open_properties.append(values[-1])
+                elif(freshline.startswith('}')):
+                    open_properties.pop()
+                else:
+                    raise ValueError("Unexpected beginning character. Line " + str(line_num) + ".")
+                    
+            if not open_properties:
+                raise ValueError("Too many closing brackets. Line " + str(line_num) + ".")
+        if len(open_properties) > 1:
+            raise ValueError("End of text reached with remaining open sections.")
+            
+        return open_properties[0].value
+    
+    def __str__(self):
+        return '\n'.join(self.to_strings())
+    
+    def to_strings(self):
+        '''Returns a list of strings that represents the property as it appears in the file.'''
+        out_val = ['"{}"'.format(self.name)]
+    
+        if instanceof(self.value, list):
+            out_val.append('{')
+            out_val.extend(['\t'+line for property in self.value for line in property.to_strings()])
+            out_val.append('}')
         else:
-          raise ValueError("Unexpected beginning character. Line " + str(line_num) + ".")
-      if not open_properties:
-        raise ValueError("Too many closing brackets. Line " + str(line_num) + ".")
-    if len(open_properties) > 1:
-        raise ValueError("End of text reached with remaining open sections.")
-    return open_properties[0].value
-    
-  @staticmethod
-  def __clean_line__(dirty_line):
-    line = dirty_line.strip()
-    if line.startswith("\\\\") or line.startswith("//"):
-      line = ""
-    # TODO: Actually strip comments off of the end of all lines
-    return line
-    
-  def to_strings(self):
-    '''Returns a string list representation of what the property appears as in the file.'''
-    out_val = ['{0}{1}{2}'.format('"', self.name, '"')]
-    
-    if self.value is []: 
-      out_val.append('{')
-      for property in self.value:
-        for line in property.to_string():
-          out_val.append('\t' + line)
-      out_val.append('}')
-    else:
-      out_val[0] += ' "' + self.value + '"'
-    return out_val
+            out_val[0] += ' "' + self.value + '"'
+            
+        return out_val
---
+++ utils.py
@@ -0,0 +1,6 @@
+def __clean_line__(dirty_line):
+    line = dirty_line.strip()
+    if line.startswith("\\\\") or line.startswith("//"):
+        line = ""
+    # TODO: Actually strip comments off of the end of all lines
+    return line
