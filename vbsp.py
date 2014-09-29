9175fb112d7f389be1efd73b26b72a2e11d4d024
TeamSpen210 <spencerb21@live.com>
2014-09-30 09:56:30 +1000

    Make vbsp_styles run vbsp_original, read arguments
    - The ingame progress bar for this isn't working properly, need to fix
    that.
    - Also includes code making flip/angled panels able to have unique
    textures only for them.

--- vbsp.py
+++ vbsp.py
@@ -1,10 +1,12 @@
 import os
 import os.path
 import sys
+import subprocess
+import shutil
+import random
 
 from property_parser import Property
 import utils
-import random
 
 instances=[] # All instances
 overlays=[]
@@ -62,7 +64,21 @@ TEX_VALVE = { # all the textures produced by the Puzzlemaker, and their replacem
     "effects/laserplane"                 : "laserfield",
     "tools/toolsnodraw"                  : "nodraw" # Don't know why someone would want to change this, but anyway...
     }
-ANTLINES = {
+WHITE_PAN = ["tile/white_wall_tile003a", 
+             "tile/white_wall_tile003h", 
+             "tile/white_wall_tile003c", 
+             "tile/white_wall_tile003f", 
+             "tile/white_floor_tile002a"
+            ]
+BLACK_PAN = [
+             "metal/black_floor_metal_001c", 
+             "metal/black_wall_metal_002c", 
+             "metal/black_wall_metal_002e", 
+             "metal/black_wall_metal_002a", 
+             "metal/black_wall_metal_002b"
+            ]
+
+ANTLINES = {                              
     "signage/indicator_lights/indicator_lights_floor" : "antline",
     "signage/indicator_lights/indicator_lights_corner_floor" : "antlineCorner"
     } # these need to be handled seperately to accomadate the scale-changing
@@ -90,7 +106,6 @@ fizzler_angle_fix = {
     "-90 180 0" : "90 0 0",
     "0 -90 -90" : "0 90 90"
     }
-print(sys.argv)
     
 def alter_mat(prop):
     if prop.value.casefold() in TEX_VALVE: # should we convert it?
@@ -120,12 +135,8 @@ def load_settings():
         if not ANTLINES[mat] in settings:
             settings[ANTLINES[mat]]=[mat]
     
-def load_map():
+def load_map(path):
     global map
-    path = sys.argv
-    print(path)
-    path="preview_test.vmf"
-    #path="F:\SteamLibrary\SteamApps\common\Portal 2\sdk_content\maps\preview.vmf"
     with open(path, "r") as file:
         print("Parsing Map...")
         map=Property.parse(file)
@@ -152,12 +163,13 @@ def load_entities():
             instances.append(item)
         elif item.cls=="info_overlay":
             overlays.append(item)
-        elif item.cls=="func_detail" or item.cls=="func_rotating": # 2nd is not detail, but it's the easiest way to convert those textures.
+        elif item.cls=="func_detail":
             detail.append(item)
         elif item.cls in ("trigger_portal_cleanser", "trigger_hurt", "trigger_multiple"):
             triggers.append(item)
-        elif item.cls=="func_brush":
+        elif item.cls in ("func_brush" , "func_door_rotating"):
             f_brushes.append(item)
+            print("Class: "+item.cls)
         else:
             other_ents.append(item)
     
@@ -222,12 +234,20 @@ def change_func_brush():
     print("Editing Brush Entities...")
     for brush in f_brushes:
         sides=Property.find_all(brush, 'entity"solid"side"material')
-        for mat in sides:
-            alter_mat(mat)
+        for mat in sides: # Func_brush/func_rotating -> angled panels and flip panels often use different textures, so let the style do that.
+            print("Mat is: "+ mat.value.casefold())
+            if mat.value.casefold() == "anim_wp/framework/squarebeams" and "edge_special" in settings:
+                mat.value = random.choice(settings["edge_special"])
+            elif mat.value.casefold() in WHITE_PAN and "white_special" in settings:
+                mat.value = random.choice(settings["white_special"])
+            elif mat.value.casefold() in BLACK_PAN and "black_special" in settings:
+                mat.value = random.choice(settings["black_special"])
+            else:
+                alter_mat(mat) # for gratings, laserfields and some others
+            
         fast_ref = Property.find_all(brush, 'entity"drawInFastReflection')
         if len(fast_ref) == 1:
             fast_ref[0].value = settings["force_brush_reflect"][0]
-            print(fast_ref[0].value)
         else:
             brush.value.append(Property("drawinfastreflection", settings["force_brush_reflect"][0]))
             
@@ -267,6 +287,30 @@ def save():
     with open("F:\SteamLibrary\SteamApps\common\Portal 2\sdk_content\maps\styled\preview.vmf", 'w') as f:
         f.writelines(out)
     print("Complete!")
-        
-for func in (load_settings, load_map, load_entities, change_brush, change_overlays, change_trig, change_func_brush, change_ents, fix_inst, save):
-    func()
+    
+def run_vbsp(args, compile_loc, target):
+    "Execute the original VBSP, copying files around so it works correctly."
+    # TODO: Get the ingame VBSP progress bar to work right. Probably involves making sure P2 gets the VBSP console output.
+    subprocess.call([os.path.join(os.getcwd(),"vbsp_original")] + args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    shutil.copy(compile_loc.replace(".vmf",".bsp"), target.replace(".vmf",".bsp"))
+    shutil.copy(compile_loc.replace(".vmf",".prt"), target.replace(".vmf",".prt")) # copy over the real files so vvis/vrad can read them
+    
+args = " ".join(sys.argv)
+new_args=sys.argv[1:]
+new_path=""
+path=""
+for i,a in enumerate(new_args):
+    if "sdk_content\\maps/" in a:
+        new_args[i] = a.replace("sdk_content\\maps/","sdk_content/maps/styled/",1)
+        new_path=new_args[i]
+        path=a
+print("Map path is " + path)
+if "-entity_limit 1750" in args: # PTI adds this, we know it's a map to convert!
+    print("PeTI map detected! (has Entity Limit of 1750)")
+    load_map(path)
+    for func in (load_settings, load_entities, change_brush, change_overlays, change_trig, change_func_brush, change_ents, fix_inst, save):
+        func()
+    run_vbsp(new_args, new_path, path)
+else:
+    print("Hammer map detected! skipping conversion..")
+    run_vbsp(sys.argv[1:], path, path)
