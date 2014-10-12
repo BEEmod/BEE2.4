c4c3695b85c68d9577d7a067d8456da4350da6a3
TeamSpen210 <spencerb21@live.com>
2014-10-12 12:56:02 +1000

    Make VRAD work
    It needs quotes on the program filenames!

--- vrad.py
+++ vrad.py
@@ -10,14 +10,17 @@ import utils
 
 def log(text):
     print(text, flush=True)
+    
+def quote(txt):
+    return '"' + txt + '"'
 
 def run_vrad(args):
     "Execute the original VRAD."
     args = [('"' + x + '"' if " " in x else x) for x in args]
-    arg = os.path.join(os.getcwd(),"vrad_original") + " " + " ".join(args)
+    arg = '"' + os.path.normpath(os.path.join(os.getcwd(),"vrad_original")) + '" ' + " ".join(args)
     log("Calling original VRAD...")
     log(arg)
-    subprocess.call(arg)
+    subprocess.call(arg, stdout=None, stderr=subprocess.PIPE, shell=True)
     log("Done!")   
 
 # MAIN
@@ -35,9 +38,11 @@ next_is_game = False
 for a in list(new_args):
     if next_is_game:
         next_is_game = False
-        game_dir = a
+        game_dir = os.path.normpath(a)
+        new_args[new_args.index(a)] = game_dir
     elif "sdk_content\\maps\\" in os.path.normpath(a):
-        path=a
+        path=os.path.normpath(a)
+        new_args[new_args.index(a)]=path
     elif a == "-game":
         next_is_game = True
     elif a.casefold() in ("-both", "-final", "-staticproplighting", "-staticproppolys", "-textureshadows"):
@@ -52,30 +57,32 @@ log("Map path is " + path)
 if path == "":
     raise Exception("No map passed!")
     
-if not path.endswith(".vmf"):
-    path += ".vmf"
+if not path.endswith(".bsp"):
+    path += ".bsp"
 
-if os.path.basename(path) == "preview.vmf": # Is this a PeTI map?
-    log("PeTI map detected! (is named preview.vmf")
+if os.path.basename(path) == "preview.bsp": # Is this a PeTI map?
+    log("PeTI map detected! (is named preview.vmf)")
     run_vrad(new_args)
 else:
     log("Hammer map detected! Not forcing cheap lighting..")
     run_vrad(sys.argv[1:])
     
-name = path[:-4]
-pack_file = name + '.filelist.txt'
+pack_file = path[:-4] + '.filelist.txt'
 
 if os.path.isfile(pack_file):
     log("Pack list found, packing files!")
-    arg_bits = [os.path.join(os.getcwd(),"bspzip"),
+    arg_bits = [quote(os.path.normpath(os.path.join(os.getcwd(),"bspzip"))),
             "-addlist",
-            '"' + name + '.bsp"',
-            '"' + pack_file + '"',
-            '"' + name + '.bsp"',
+            quote(path),
+            quote(pack_file),
+            quote(path),
             "-game",
-            '"' +game_dir + '"',
+            quote(game_dir),
           ]
     arg = " ".join(arg_bits)
     log(arg)
-    subprocess.call(arg)
+    subprocess.call(arg, stdout=None, stderr=subprocess.PIPE, shell=True)
     log("Packing complete!")
+else:
+    log("No items to pack!")
+log("BEE2 VRAD hook finished!")
