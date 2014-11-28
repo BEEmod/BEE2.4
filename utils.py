0a8a9050deae4cd1eb1d36c73ea2ecd76a4a29dc
TeamSpen210 <spencerb21@live.com>
2014-11-28 17:13:51 +1000

    Better Vector docstrings and some bugfixes
    - __sub__ actually works
    - norm() uses mag() instead of len() to allow it to work with decimal
    magnitudes.

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import math
+import collections.abc as abc
 
 from property_parser import Property
 
@@ -29,11 +30,31 @@ class Vec:
     "A 3D Vector. This has most standard Vector functions."
     __slots__ = ('x', 'y', 'z')
     
-    def __init__(self, x = 0, y=0, z=0):
-        if isinstance(x, tuple) or isinstance(x, list):
-            self.x = float(x[0])
-            self.y = float(x[1])
-            self.z = float(x[2])
+    def __init__(self, x=0, y=0, z=0):
+        '''Create a Vector.
+        
+        All values are converted to Floats automatically.
+        If no value is given, that axis will be set to 0.
+        A tuple can be passed in (as the x argument), which will use the three args as 
+        '''
+        if isinstance(x, abc.Sequence):
+            ln = len(x)
+            if ln>=3:
+                self.x = float(x[0])
+                self.y = float(x[1])
+                self.z = float(x[2])
+            elif ln>=2:
+                self.x = float(x[0])
+                self.y = float(x[1])
+                self.z = 0
+            elif ln>=1:
+                self.x = float(x[0])
+                self.y = 0
+                self.z = 0
+            else:
+                self.x=0
+                self.y=0
+                self.z=0
         else:
             self.x = float(x)
             self.y = float(y)
@@ -54,7 +75,7 @@ class Vec:
     def __sub__(self,other):
         "- operation. This works on numbers (adds to all axes), or other Vectors (adds to just our one)."
         if isinstance(other, Vec):
-            return Vec(self.x-other.x, self.y-other.y, self.y+other.y, self.z+other.z)
+            return Vec(self.x-other.x, self.y-other.y, self.z-other.z)
         else:
             return Vec(self.x-other, self.y-other)
             
@@ -140,6 +161,12 @@ class Vec:
             return self
             
     def __eq__(self, other):
+        '''Equality test. 
+        
+        Two Vectors are compared based on the axes.
+        A Vector can be compared with a 3-tuple as if it was a Vector also.
+        Otherwise the other value will be compared with the magnitude.
+        '''
         if isinstance(other, Vec):
             return other.x == self.x and other.y==self.y and other.z == self.z
         elif isinstance(other, tuple):
@@ -151,9 +178,15 @@ class Vec:
                 return NotImplemented
                 
     def __lt__(self, other):
+        '''A<B test. 
+        
+        Two Vectors are compared based on the axes.
+        A Vector can be compared with a 3-tuple as if it was a Vector also.
+        Otherwise the other value will be compared with the magnitude.
+        '''
         if isinstance(other, Vec):
             return self.x < other.x and self.y < other.y and self.z < other.z
-        elif isinstance(other, tuple):
+        elif isinstance(other, abc.Sequence):
             return self.x < other[0] and self.y < other[1] and self.z < other[2]
         else:
             try:
@@ -162,9 +195,15 @@ class Vec:
                 return NotImplemented
                 
     def __le__(self, other):
+        '''A<=B test. 
+        
+        Two Vectors are compared based on the axes.
+        A Vector can be compared with a 3-tuple as if it was a Vector also.
+        Otherwise the other value will be compared with the magnitude.
+        '''
         if isinstance(other, Vec):
             return self.x <= other.x and self.y <= other.y and self.z <= other.z
-        elif isinstance(other, tuple):
+        elif isinstance(other, abc.Sequence):
             return self.x <= other[0] and self.y <= other[1] and self.z <= other[2]
         else:
             try:
@@ -173,9 +212,15 @@ class Vec:
                 return NotImplemented
                 
     def __gt__(self, other):
+        '''A>B test. 
+        
+        Two Vectors are compared based on the axes.
+        A Vector can be compared with a 3-tuple as if it was a Vector also.
+        Otherwise the other value will be compared with the magnitude.
+        '''
         if isinstance(other, Vec):
             return self.x>other.x and self.y.other.y and self.z<other.z
-        elif isinstance(other, tuple):
+        elif isinstance(other, abc.Sequence):
             return self.x>other[0] and self.y>other[1] and self.z>other[2]
         else:
             try:
@@ -203,17 +248,24 @@ class Vec:
             self.z = other.z
                 
     def mag(self):
+        '''Compute the distance from the vector and the origin.'''
         if self.z == 0:
             return math.sqrt(self.x**2+self.y**2)
         else:
             return math.sqrt(self.x**2 + self.y**2 + self.z**2)
             
     def __len__(self):
-        "Length gives the magnitude."
+        '''len(x) gives the magnitude of the vector.
+        
+        NOTE: This gives an integer, so using .mag() is far more desirable.
+        '''
         return int(self.mag())
         
     def join(self, delim=', '):
-        "Return a string with all numbers joined by the passed delimiter."
+        '''Return a string with all numbers joined by the passed delimiter.
+        
+        This strips off the .0 if no decimal portion exists.
+        '''
         if self.x.is_integer():
             x = int(self.x)
         else:
@@ -248,7 +300,11 @@ class Vec:
         yield self.z
         
     def __getitem__(self, ind):
-        "Allow referencing by index instead of name if desired."
+        '''Allow reading values by index instead of name if desired.
+        
+        This accepts either 0,1,2 or 'x','y','z' to read values.
+        Useful in conjunction with a loop to apply commands to all values.
+        '''
         if ind == 0 or ind == "x":
             return self.x
         elif ind == 1 or ind == "y":
@@ -259,7 +315,11 @@ class Vec:
             return NotImplemented
     
     def __setitem__(self, ind, val):
-        "Allow referencing by index instead of name if desired."
+        '''Allow editing values by index instead of name if desired.
+        
+        This accepts either 0,1,2 or 'x','y','z' to edit values.
+        Useful in conjunction with a loop to apply commands to all values.
+        '''
         if ind == 0 or ind == "x":
             self.x = float(val)
         elif ind == 1 or ind == "y":
@@ -270,7 +330,7 @@ class Vec:
             return NotImplemented
             
     def as_tuple(self):
-        "return the Vector as a tuple."
+        "Return the Vector as a tuple."
         return (self.x, self.y, self.z)
             
     def len_sq(self):
@@ -281,15 +341,20 @@ class Vec:
             return self.x**2 + self.y**2 + self.z**2
             
     def keys(self):
-        return "x", "y", "z"
+        '''Return the three axes.
+        
+        Useful in conjunction with a for loop, to execute code on the x/y/z components.
+        '''
+        return ("x", "y", "z")
         
     def norm(self):
         "Normalise the Vector by transforming it to have a magnitude of 1 but the same direction."
+        print(self.x, self.y, self.z)
         if self.x == 0 and self.y==0 and self.z == 0:
             # Don't do anything for this, and don't copy
             return self
         else:
-            return self / len(self)
+            return self / self.mag()
     
     def dot(a, b):
         "Return the dot product of both Vectors."
