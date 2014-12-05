33d38ce40911b1a6682adab19e9477922e44c7dd
TeamSpen210 <spencerb21@live.com>
2014-12-05 12:47:53 +1000

    Fix cx_freeze command
    You can't have 2 setup() commands, they overwrite each other's
    libary.zip

--- compile_vbsp_vrad.py
+++ compile_vbsp_vrad.py
@@ -3,14 +3,9 @@ import os
 
 ico_path = os.path.join(os.getcwd(), "bee2.ico")
 
-setup(name='VBSP',
+setup(name='VBSP_VRAD',
       version = '0.1',
       options = {'build_exe': {'build_exe':'build_compiler'}},
-      description = 'BEE2 VBSP replacement',
-      executables = [Executable('vbsp.py', base='Console', icon=ico_path)])
-      
-setup(name='VRAD',
-      version = '0.1',
-      options = {'build_exe': {'build_exe':'build_compiler'}},
-      description = 'BEE2 VRAD replacement',
-      executables = [Executable('vrad.py', base='Console', icon=ico_path)])
+      description = 'BEE2 VBSP and VRAD compilation hooks, for modifying PeTI maps during compilation.',
+      executables = [Executable('vbsp.py', base='Console', icon=ico_path),
+                     Executable('vrad.py', base='Console', icon=ico_path)])
