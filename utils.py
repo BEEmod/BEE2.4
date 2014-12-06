256e016f2d81c5c871e42cf79d5e35f806b0d43a
TeamSpen210 <spencerb21@live.com>
2014-12-07 09:54:44 +1000

    Improve clean_line().
    - Comments don't ever use '\\'
    - It now removes comments on the ends of lines

--- utils.py
+++ utils.py
@@ -3,18 +3,16 @@ import collections.abc as abc
 
 from property_parser import Property
 
-def clean_line(dirty_line):
+def clean_line(line):
     "Removes extra spaces and comments from the input."
-    if isinstance(dirty_line, bytes):
-        dirty_line=dirty_line.decode() # convert bytes to strings if needed
-    line = dirty_line.strip()
-    if line.startswith("\\\\") or line.startswith("//"):
-        line = ""
-    # TODO: Actually strip comments off of the end of all lines
-    return line
+    if isinstance(line, bytes):
+        line=line.decode() # convert bytes to strings if needed
+    if '//' in line:
+        line = line.split('//', 1)[0]
+    return line.strip()
     
 def is_identifier(name, forbidden='{}\'"'):
-    "Check to see if any forbidden characters are part of a canidate name."
+    "Check to see if any forbidden characters are part of a candidate name."
     for t in name:
         if t in forbidden:
             return False
