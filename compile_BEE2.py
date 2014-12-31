d0fd2bd565b0a58990a0d9fe2e1b8cb8303d8b2d
TeamSpen210 <spencerb21@live.com>
2015-01-01 09:06:00 +1000

    Switch BEE2 to .pyw file
    This does not produce any console window.

--- compile_BEE2.py
+++ compile_BEE2.py
@@ -7,4 +7,4 @@ setup(name='BEE2',
       version = '2.4',
       description = 'Portal 2 Puzzlemaker item manager.',
       options = {'build_exe': {'build_exe':'build_BEE2', 'include_files': ['bee2.ico']}},
-      executables = [Executable('BEE2.py', base='Win32GUI', icon=ico_path, compress=True)])
+      executables = [Executable('BEE2.pyw', base='Win32GUI', icon=ico_path, compress=True)])
