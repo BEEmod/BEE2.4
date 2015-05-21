66ed9382f2103c011ab87abcae79d60db8a6e346
TeamSpen210 <spencerb21@live.com>
2015-05-21 17:24:30 +1000

    Tidy up compile scripts, fix config.py to work with VBSP

--- compile_BEE2.py
+++ compile_BEE2.py
@@ -1,10 +1,26 @@
 from cx_Freeze import setup, Executable
-import os
+import os, shutil
+
+shutil.rmtree('build_BEE2', ignore_errors=True)
 
 ico_path = os.path.join(os.getcwd(), "bee2.ico")
 
-setup(name='BEE2',
-      version = '2.4',
-      description = 'Portal 2 Puzzlemaker item manager.',
-      options = {'build_exe': {'build_exe':'build_BEE2', 'include_files': ['bee2.ico']}},
-      executables = [Executable('BEE2.pyw', base='Win32GUI', icon=ico_path, compress=True)])
+setup(
+    name='BEE2',
+    version='2.4',
+    description='Portal 2 Puzzlemaker item manager.',
+    options={
+        'build_exe': {
+            'build_exe': 'build_BEE2/bin',
+            'include_files': ['bee2.ico'],
+        },
+    },
+    executables=[
+        Executable(
+            'src/BEE2.pyw',
+            base='Win32GUI',
+            icon=ico_path,
+            compress=True,
+        )
+    ],
+)
--- compile_vbsp_vrad.py
+++ compile_vbsp_vrad.py
@@ -5,7 +5,7 @@ ico_path = os.path.join(os.getcwd(), "bee2.ico")
 
 setup(name='VBSP_VRAD',
       version = '0.1',
-      options = {'build_exe': {'build_exe':'build_compiler'}},
+      options = {'build_exe': {'build_exe': 'build_compiler'}},
       description = 'BEE2 VBSP and VRAD compilation hooks, for modifying PeTI maps during compilation.',
-      executables = [Executable('vbsp.py', base='Console', icon=ico_path),
-                     Executable('vrad.py', base='Console', icon=ico_path)])
+      executables = [Executable('src/vbsp.py', base='Console', icon=ico_path),
+                     Executable('src/vrad.py', base='Console', icon=ico_path)])
