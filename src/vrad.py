15a991543008587272c28cb69ca3549545298577
TeamSpen210 <spencerb21@live.com>
2015-09-04 17:07:30 +1000

    Move VRAD logic into main() function

--- src/vrad.py
+++ src/vrad.py
@@ -10,6 +10,26 @@ def quote(txt):
     return '"' + txt + '"'
 
 
+def pack_content(path):
+    """Pack any custom content into the map."""
+    utils.con_log("Running PakRat!")
+    arg_bits = [
+        quote(os.path.normpath(os.path.join(os.getcwd(), "bee2/pakrat.jar"))),
+        "-auto",
+        quote(os.path.normpath(
+            os.path.join(
+                os.path.dirname(os.getcwd()),
+                'bee2/',
+            )
+        )),
+        quote(path),
+    ]
+    arg = " ".join(arg_bits)
+    utils.con_log(arg)
+    subprocess.call(arg, stdout=None, stderr=subprocess.PIPE, shell=True)
+    utils.con_log("Packing complete!")
+
+
 def run_vrad(args):
     "Execute the original VRAD."
 
@@ -45,85 +65,75 @@ def run_vrad(args):
         utils.con_log("VRAD failed! (" + str(code) + ")")
         sys.exit(code)
 
-# MAIN
-to_pack = []  # the file path for any items that we should be packing
-to_pack_inst = {}  # items to pack for a specific instance
-to_pack_mat = {}  # files to pack if material is used (by VBSP_styles only)
 
-TK_ROOT = os.path.dirname(os.getcwd())
-args = " ".join(sys.argv)
-fast_args = sys.argv[1:]
-full_args = sys.argv[1:]
+def main(argv):
+    utils.con_log('BEE2 VRAD hook started!')
+    args = " ".join(argv)
+    fast_args = argv[1:]
+    full_args = argv[1:]
 
-path = sys.argv[-1]  # The path is the last argument to vrad
-fast_args[-1] = os.path.normpath(path)
+    path = argv[-1]  # The path is the last argument to vrad
+    fast_args[-1] = os.path.normpath(path)
 
-for a in fast_args[:]:
-    if a.casefold() in (
-            "-both",
-            "-final",
-            "-staticproplighting",
-            "-staticproppolys",
-            "-textureshadows",
-            ):
-        # remove final parameters from the modified arguments
-        fast_args.remove(a)
-    elif a in ('-force_peti', '-force_hammer', '-no_pack'):
-        # we need to strip these out, otherwise VBSP will get confused
-        fast_args.remove(a)
-        full_args.remove(a)
+    utils.con_log("Map path is " + path)
+    if path == "":
+        raise Exception("No map passed!")
 
-fast_args = ['-bounce', '2', '-noextra'] + fast_args
 
-# Fast args: -bounce 2 -noextra -game $gamedir $path\$file
-# Final args: -both -final -staticproplighting -StaticPropPolys
-# -textureshadows  -game $gamedir $path\$file
+    for a in fast_args[:]:
+        if a.casefold() in (
+                "-both",
+                "-final",
+                "-staticproplighting",
+                "-staticproppolys",
+                "-textureshadows",
+                ):
+            # remove final parameters from the modified arguments
+            fast_args.remove(a)
+        elif a in ('-force_peti', '-force_hammer', '-no_pack'):
+            # we need to strip these out, otherwise VBSP will get confused
+            fast_args.remove(a)
+            full_args.remove(a)
 
-utils.con_log("Map path is " + path)
-if path == "":
-    raise Exception("No map passed!")
+    fast_args = ['-bounce', '2', '-noextra'] + fast_args
 
-if not path.endswith(".bsp"):
-    path += ".bsp"
+    # Fast args: -bounce 2 -noextra -game $gamedir $path\$file
+    # Final args: -both -final -staticproplighting -StaticPropPolys
+    # -textureshadows  -game $gamedir $path\$file
 
-if '-force_peti' in args or '-force_hammer' in args:
-    # we have override command!
-    if '-force_peti' in args:
-        utils.con_log('OVERRIDE: Applying cheap lighting!')
-        is_peti = True
+    if not path.endswith(".bsp"):
+        path += ".bsp"
+
+    if '-force_peti' in args or '-force_hammer' in args:
+        # we have override command!
+        if '-force_peti' in args:
+            utils.con_log('OVERRIDE: Applying cheap lighting!')
+            is_peti = True
+        else:
+            utils.con_log('OVERRIDE: Preserving args!')
+            is_peti = False
     else:
-        utils.con_log('OVERRIDE: Preserving args!')
-        is_peti = False
-else:
-    # If we don't get the special -force args, check for the name
-    # equalling preview to determine if we should convert
-    is_peti = os.path.basename(path) == "preview.bsp"
-if is_peti:
-    utils.con_log("PeTI map detected!")
-    run_vrad(fast_args)
-else:
-    utils.con_log("Hammer map detected! Not forcing cheap lighting..")
-    run_vrad(full_args)
+        # If we don't get the special -force args, check for the name
+        # equalling preview to determine if we should convert
+        # If that is false, check the config file to see what was
+        # specified there.
+        is_peti = (
+            os.path.basename(path) == "preview.bsp" or
+        )
 
-pack_file = path[:-4] + '.filelist.txt'
 
-if '-no_pack' not in args:
-    utils.con_log("Running PakRat!")
-    arg_bits = [
-        quote(os.path.normpath(os.path.join(os.getcwd(), "bee2/pakrat.jar"))),
-        "-auto",
-        quote(os.path.normpath(
-            os.path.join(
-                os.path.dirname(os.getcwd()),
-                'bee2/',
-            )
-        )),
-        quote(path),
-    ]
-    arg = " ".join(arg_bits)
-    utils.con_log(arg)
-    subprocess.call(arg, stdout=None, stderr=subprocess.PIPE, shell=True)
-    utils.con_log("Packing complete!")
-else:
-    utils.con_log("No items to pack!")
-utils.con_log("BEE2 VRAD hook finished!")
+    if is_peti:
+        utils.con_log("Forcing Cheap Lighting!")
+        run_vrad(fast_args)
+    else:
+        utils.con_log("Hammer map detected! Not forcing cheap lighting..")
+        run_vrad(full_args)
+
+    if '-no_pack' not in args:
+        pack_content(path)
+    else:
+        utils.con_log("No items to pack!")
+    utils.con_log("BEE2 VRAD hook finished!")
+
+if __name__ == '__main__':
+    main(sys.argv)
