f6be22dd2973e7237ea833cd115616142bc34f7f
TeamSpen210 <spencerb21@live.com>
2015-11-09 17:34:45 +1000

    Add some additional white tiles to the list

--- src/conditions.py
+++ src/conditions.py
@@ -143,6 +143,8 @@ TEMPLATE_RETEXTURE = {
 
     'tile/white_wall_tile001a': (W, 'wall'),
     'tile/white_wall_tile003a': (W, 'wall'),
+    'tile/white_wall_tile003b': (W, 'wall'),
+    'tile/white_wall_tile003c': (W, '2x2'),
     'tile/white_wall_tile003h': (W, 'wall'),
     'tile/white_wall_state': (W, '2x2'),
     'tile/white_wall_tile003f': (W, '4x4'),
