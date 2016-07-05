af35db3021db81d2b142dd684c91d5d948374bcc
TeamSpen210 <spencerb21@live.com>
2016-07-05 10:53:54 +1000

    Read the map to set block positions

--- src/brushLoc.py
+++ src/brushLoc.py
@@ -2,23 +2,26 @@
 
 """
 from collections import deque
+
 from srctools import Vec, Vec_tuple, Property, Entity, VMF
 from enum import Enum
 
 import utils
+import vbsp
+import bottomlessPit
 
 from typing import Dict, Union
 
 LOGGER = utils.getLogger(__name__)
 
-def world_to_grid(pos: Vec):
+def world_to_grid(pos: Vec) -> Vec:
     """Given real coordinates, find the grid position."""
     return pos // 128
 
 
-def grid_to_world(pos: Vec):
+def grid_to_world(pos: Vec) -> Vec:
     """Given a grid position, find the center of the real block."""
-    pos * 128 + (64, 64, 64)
+    return pos * 128 + (64, 64, 64)
 
 
 class Block(Enum):
@@ -38,6 +41,20 @@ class Block(Enum):
     PIT_TOP = 21  # Top of a bottomless pit
     PIT_MID = 22  # Middle of a bottomless pit
     PIT_BOTTOM = 23  # Base of a bottomless pit
+
+    @classmethod
+    def from_pitgoo_attr(cls, is_pit: bool, is_top: bool, is_bottom: bool):
+        """Return the appropriate enum, based on bool parameters."""
+        off = 20 if is_pit else 10
+        if is_top:
+            if is_bottom:
+                return cls(off)  # Single
+            else:
+                return cls(off + 1)  # top
+        elif is_bottom:
+            return cls(off + 3)  # Bottom
+        else:
+            return cls(off + 2)  # Mid
 
     @property
     def inside_map(self):
@@ -99,8 +116,132 @@ class Grid(Dict[_grid_keys, Block]):
         yield from map(Vec, super().keys())
 
     def items(self):
-        for pos, block in super.items():
+        for pos, block in super().items():
             yield Vec(pos), block
+
+    def read_from_map(self, vmf: VMF):
+        """Given the map file, set blocks."""
+        search_locs = []
+
+        for ent in vmf.entities:
+            pos = ent['origin', None]
+            if pos is None:
+                continue
+            pos = world_to_grid(Vec.from_str(pos))
+
+            # Exclude entities outside the main area - elevators mainly.
+            # The border should never be set to air!
+            if (0, 0, 0) <= pos <= (25, 25, 25):
+                search_locs.append(pos)
+
+        can_have_pit = bottomlessPit.pits_allowed()
+
+        for brush in vmf.brushes[:]:
+            tex = {face.mat.casefold() for face in brush.sides}
+
+            bbox_min, bbox_max = brush.get_bbox()
+
+            if ('nature/toxicslime_a2_bridge_intro' in tex or
+                'nature/toxicslime_puzzlemaker_cheap' in tex):
+                # It's goo!
+
+                x = bbox_min.x + 64
+                y = bbox_min.y + 64
+
+                g_x = x // 128
+                g_y = y // 128
+
+                is_pit = can_have_pit and bottomlessPit.is_pit(bbox_min, bbox_max)
+
+                # If goo is multi-level, we want to record all pos!
+                z_pos = range(int(bbox_min.z) + 64, int(bbox_max.z), 128)
+                top_ind = len(z_pos) - 1
+                for ind, z in enumerate(z_pos):
+                    g_z = z // 128
+                    self[g_x, g_y, g_z] = Block.from_pitgoo_attr(
+                        is_pit,
+                        is_top=(ind == top_ind),
+                        is_bottom=(ind == 0),
+                    )
+                    # If goo has totally submerged tunnels, they are not filled.
+                    # Add each horizontal neighbour to the search list.
+                    # If not found they'll be ignored.
+                    if ind != top_ind: # Don't bother on the top level..
+                        search_locs.extend([
+                            (g_x - 1, g_y, g_z),
+                            (g_x + 1, g_y, g_z),
+                            (g_x, g_y + 1, g_z),
+                            (g_x, g_y - 1, g_z),
+                        ])
+
+                # Bottomless pits don't use goo, so remove the water..
+                if is_pit:
+                    vmf.remove_brush(brush)
+
+                # Indicate that this map contains goo/pits
+                vbsp.settings['has_attr']['pit' if is_pit else 'goo'] = True
+
+                continue
+
+            pos = world_to_grid(brush.get_origin(bbox_min, bbox_max))
+
+            if bbox_max - bbox_min == (128, 128, 128):
+                # Full block..
+                self[pos] = Block.SOLID
+            else:
+                # Must be an embbedvoxel block
+                self[pos] = Block.EMBED
+
+        LOGGER.info(
+            'Analysed map, filling air... ({} starting positions..)',
+            len(search_locs)
+        )
+        self.fill_air(search_locs)
+        LOGGER.info('Air filled!')
+
+    def fill_air(self, search_locs):
+        """Flood-fill the area, making all inside spaces air.
+
+        This assumes the map is sealed.
+        We start by assuming all instance positions are air.
+        Since ambient_light ents are placed every 5 blocks, this should
+        cover all playable space.
+        """
+        queue = deque(search_locs)
+
+        def iterdel():
+            """Iterate as FIFO queue, deleting as we go."""
+            try:
+                while True:
+                    yield Vec(queue.popleft())
+            except IndexError:  # We're empty!
+                return
+
+        # This will iterate every item we add to the queue..
+        for pos in iterdel():  # type: Vec
+            if pos in self:
+                # Already set...
+                continue
+
+            # We got outside the map somehow?
+            # There's a buffer region since large embedded areas may
+            # be interpreted as small air pockets, that's fine.
+            if not ((-15, -15, -15) <= pos <= (40, 40, 40)):
+                LOGGER.warning('Attempted leak at {}', pos)
+                continue
+
+            self[pos] = Block.AIR
+            x, y, z = pos
+            # Continue filling in each other direction.
+            queue.extend([
+                (x, y + 1, z),
+                (x, y - 1, z),
+                (x + 1, y, z),
+                (x - 1, y, z),
+                (x, y, z + 1),
+                (x, y, z - 1),
+            ])
+
     def dump_to_map(self, vmf: VMF):
         """Debug purposes: Dump the info as entities in the map.
 
@@ -129,6 +270,7 @@ class Grid(Dict[_grid_keys, Block]):
                 targetname=block.name.title(),
                 classname=block_icons[block],
                 origin=grid_to_world(pos),
+                pos=str(pos),
             )
 
 
