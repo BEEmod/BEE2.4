f1a6e7721a6295e3edb5e5f344abc99dd85f4b26
TeamSpen210 <spencerb21@live.com>
2016-07-22 18:21:34 +1000

    Detect markers, and start with the decorating process

--- src/decoration.py
+++ src/decoration.py
@@ -1,14 +1,19 @@
 """Randomly decorates unused blocks with props and missing tiles.
 
 """
+from collections import defaultdict
 from enum import Enum
 
-import utils
 from brushLoc import Block, POS
 from srctools import Property, Vec, EmptyMapping
 from srctools.vmf import Entity, EntityFixup
+from conditions import make_result, meta_cond
+import utils
 
-from typing import Union, Tuple, Callable, NamedTuple
+from typing import (
+    Union, Callable,
+    Tuple, NamedTuple, List, Dict, Set
+)
 
 LOGGER = utils.getLogger(__name__, 'cond.deco')
 
@@ -23,6 +28,16 @@ Instance = NamedTuple('Instance', [
     ('offset', Vec),
     ('fixups', EntityFixup),
 ])
+
+# Locations of marker points: origin -> set(normals)
+MARKER_LOCS = defaultdict(set)  # type: Dict[Tuple[float, float, float], Set[Tuple[float, float, float]]]
+
+# Name -> extra distance in each direction.
+MARKER_SIZES = {
+    '1x1': 0,
+    '3x3': 1,
+    '5x5': 2,
+}
 
 # Strings to combinations of blocks. Either a Block, tuple, or callable.
 OCCUPY_TYPES = {
@@ -46,15 +61,29 @@ OCCUPY_TYPES = {
 
 def valid_pos(
     pos: Block,
-    occu_type: Union[Block, Tuple[Block], Callable[[Block], bool]]
+    occu_type: Union[Block, Tuple[Block], property, Callable[[Block], bool]]
 ) -> bool:
     """Checks if a OCCUPY_TYPE is valid for a position."""
-    if occu_type is pos:
-        return True
+    if isinstance(occu_type, Block):
+        return pos is occu_type
     elif isinstance(occu_type, tuple):
         return pos in occu_type
+    elif isinstance(occu_type, property):
+        return occu_type.fget(pos)
     else:
         return occu_type(pos)
+
+# Convert each value to a set of matching block types.
+# It's frozen so we can put these in dict keys.
+OCCUPY_TYPES = {
+    name: frozenset({
+        block for block in Block
+        if valid_pos(block, occu_type)
+    }) for name, occu_type in
+    OCCUPY_TYPES.items()
+}
+
+LOGGER.info(OCCUPY_TYPES)
 
 
 class ORIENT(Enum):
@@ -63,7 +92,8 @@ class ORIENT(Enum):
     CEILING = 'CEILING'
     WALLS = 'WALLS'
 
-DECORATIONS = []
+DECORATIONS = []  # type: List[Decoration]
+
 
 class Decoration:
     """Represents one decoration type."""
@@ -86,8 +116,6 @@ class Decoration:
         self.locs = dict(locs)
         self.props = list(props)
         self.instances = list(instances)
-
-        LOGGER.info('"{}": {}', name, vars(self))
 
     @classmethod
     def parse(cls, props: Property):
@@ -122,18 +150,28 @@ class Decoration:
 
         for loc_block in props.find_all('RequiredSpace'):
             for pos in loc_block:
+                try:
+                    value = OCCUPY_TYPES[pos.name.upper()]
+                except KeyError:
+                    continue
+                LOGGER.info('Req-Space: {!r} -> {}', pos, value)
                 if pos.has_children():
                     bbox_min, bbox_max = Vec.bbox(
                         pos.vec('Pos1'),
                         pos.vec('Pos2'),
                     )
-                    for x in range(int(bbox_min.x), int(bbox_max.x)):
-                        for y in range(int(bbox_min.y), int(bbox_max.y)):
-                            for z in range(int(bbox_min.z), int(bbox_max.z)):
-                                locs[x, y, z] = pos.name
+                    for x in range(int(bbox_min.x), int(bbox_max.x) + 1):
+                        for y in range(int(bbox_min.y), int(bbox_max.y) + 1):
+                            for z in range(int(bbox_min.z), int(bbox_max.z) + 1):
+                                locs[x, y, z] = value
                 else:
                     # One location
-                    locs[Vec.from_str(pos.value).as_tuple()] = pos.name
+                    locs[Vec.from_str(pos.value).as_tuple()] = value
+
+        if not locs:
+            # By default, we want the square to be air, and the floor to be solid.
+            locs[0, 0, 0] = OCCUPY_TYPES['AIR']
+            locs[0, 0, -1] = OCCUPY_TYPES['BLOCK']
 
         return cls(
             props.real_name,
@@ -147,12 +185,71 @@ class Decoration:
             instances,
         )
 
+    def __str__(self):
+        return (
+            '<Deco: {orient}, {rotation} deg, +-'
+            '({side}, {side}, {norm})>: \n{props}\n{inst}'.format(
+                orient=self.orient,
+                rotation=self.rotation,
+                side=self.side_off,
+                norm=self.norm_off,
+                props=self.props,
+                inst=self.instances
+            )
+        )
+
+
+@make_result('DecorationMarker')
+def res_deco_marker(inst: Entity, res: Property):
+    """Identify this as a decoration marker. This removes the instance."""
+    try:
+        off = MARKER_SIZES[res.value]
+    except KeyError:
+        LOGGER.error('Invalid marker size: {}', res.value)
+        return
+
+    angles = Vec.from_str(inst['angles'])
+    origin = Vec.from_str(inst['origin']) // 128
+
+    normal = Vec(0, 0, 1).rotate(*angles).as_tuple()
+
+    for x, y in utils.iter_grid(off + 1, off + 1, -off, -off):
+        pos = (origin + Vec(x, y, 0).rotate(*angles)).as_tuple()
+        # Ignore markers positioned inside walls and in void.
+        if POS[pos].traversable:
+            MARKER_LOCS[pos].add(normal)
+
+    inst.remove()
+
 
 def load_deco():
     DECORATIONS.clear()
     with open('bee2/decoration.cfg') as f:
         props = Property.parse(f)
-    for deco_block in props.find_all('Decoration'):
+    for deco_block in props.find_all('Decorations'):
         for deco in deco_block:
             DECORATIONS.append(Decoration.parse(deco))
     LOGGER.info('Loaded decoration data..')
+
+
+@meta_cond(priority=300, only_once=True)
+def place_decorations(_):
+    """Place decorations into the map."""
+    LOGGER.info(
+        'Placing decorations ({} marked spots), {} possible decorations.',
+        len(MARKER_LOCS),
+        len(DECORATIONS),
+    )
+    if not MARKER_LOCS or not DECORATIONS:
+        return
+
+    # First, we want to figure out all unique placement shapes for items.
+    placement = defaultdict(list)
+    for deco in DECORATIONS:
+        shape = frozenset({
+            (x, y, z, block)
+            for (x, y, z), block in
+            deco.locs.items()
+        })
+        placement[shape].append(deco)
+
