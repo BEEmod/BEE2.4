54e44942def4df6e4c0636562280adb44455031d
TeamSpen210 <spencerb21@live.com>
2016-08-07 16:58:26 +1000

    Add logic for conveyors

---
+++ src/conditions/conveyorBelt.py
@@ -0,0 +1,133 @@
+"""Continuously moving belts, like in BTS.
+"""
+from srctools import Property, Vec, Entity, Output
+
+import conditions
+import srctools
+
+
+@conditions.make_result('ConveyorBelt')
+def res_conveyor_belt(inst: Entity, res: Property):
+    """Create a conveyor belt.
+
+    Options:
+        SegmentInst: Generated at each square. ('track' is the name of the path.)
+        TrackTeleport: Set the track points so they teleport trains to the start.
+        Speed: The fixup or number for the train speed.
+        MotionTrig: If set, a trigger_multiple will be spawned that EnableMotions
+        weighted cubes. The value is the name of the relevant filter.
+        EndOutput: Adds an output to the last track. The value is the same as
+        outputs in VMFs.
+    """
+    move_dist = srctools.conv_int(inst.fixup['$travel_distance'])
+
+    if move_dist <= 2:
+        # There isn't room for a catwalk, so don't bother.
+        inst.remove()
+        return
+
+    move_dir = Vec(1, 0, 0).rotate_by_str(inst.fixup['$travel_direction'])
+    start_offset = srctools.conv_float(inst.fixup['$starting_position'], 0)
+    teleport_to_start = res.bool('TrackTeleport', True)
+    segment_inst_file = res['SegmentInst', '']
+
+    vmf = inst.map
+
+
+    if segment_inst_file:
+        segment_inst_file = conditions.resolve_inst(segment_inst_file)[0]
+
+    speed = srctools.conv_float(
+        conditions.resolve_value(inst, res['speed', '100'])
+    )
+
+    start_pos = Vec.from_str(inst['origin'])
+    end_pos = start_pos + move_dist * move_dir
+
+    if start_offset > 0:
+        # If an oscillating platform, move to the closest side..
+        offset = start_offset * move_dir
+        # The instance is placed this far along, so move back to the end.
+        start_pos -= offset
+        end_pos -= offset
+        if start_offset > 0.5:
+            # Swap the direction of movement..
+            start_pos, end_pos = end_pos, start_pos
+        inst['origin'] = start_pos
+
+    # Find the angle which generates an instance pointing in the direction
+    # of movement, with the same normal.
+    norm = Vec(z=1).rotate_by_str(inst['angles'])
+    for roll in range(0, 360, 90):
+        angles = move_dir.to_angle(roll)
+        if Vec(z=1).rotate(*angles) == norm:
+            break
+    else:
+        raise ValueError(
+            "Can't find angles to give a"
+            ' z={} and x={}!'.format(norm, move_dir)
+        )
+
+    inst['angles'] = angles
+
+    track_name = conditions.local_name(inst, 'segment_{}')
+    last_track = None
+    for index, pos in enumerate(start_pos.iter_line(end_pos, stride=128), start=1):
+        track = vmf.create_ent(
+            classname='path_track',
+            targetname=track_name.format(index) + '-track',
+            origin=pos,
+            spawnflags=0,
+            speed=speed,
+            orientationtype=0,  # Don't rotate
+        )
+        if last_track:
+            last_track['target'] = track['targetname']
+
+        if index == 1 and teleport_to_start:
+            track['spawnflags'] = 16  # Teleport here..
+
+        last_track = track
+
+        if segment_inst_file:
+            seg_inst = vmf.create_ent(
+                classname='func_instance',
+                targetname=track_name.format(index),
+                file=segment_inst_file,
+                origin=pos,
+                angles=inst['angles'],
+            )
+            seg_inst.fixup.update(inst.fixup)
+
+    if teleport_to_start:
+        # Link back to the first track..
+        last_track['target'] = track_name.format(1) + '-track'
+
+    # Allow adding outputs to the last path_track.
+    for prop in res.find_all('EndOutput'):
+        output = Output.parse(prop)
+        output.output = 'OnPass'
+        output.inst_out = None
+        output.comma_sep = False
+        output.target = conditions.local_name(inst, output.target)
+        last_track.add_out(output)
+
+    motion_filter = res['motionTrig', None]
+    if motion_filter is not None:
+        # Add the EnableMotion trigger_multiple seen in platform items.
+        motion_trig = vmf.create_ent(
+            classname='trigger_multiple',
+            targetname=conditions.local_name(inst, 'enable_motion_trig'),
+            origin=start_pos,
+            filtername=motion_filter,
+            startDisabled=1,
+            wait=0.1,
+        )
+        motion_trig.add_out(Output('OnStartTouch', '!activator', 'ExitDisabledState'))
+        # Match the size of the original...
+        motion_trig.solids.append(vmf.make_prism(
+            start_pos + Vec(72, -56, 58).rotate(*angles),
+            end_pos + Vec(-72, 56, 144).rotate(*angles),
+            mat='tools/toolstrigger',
+        ).solid)
+
