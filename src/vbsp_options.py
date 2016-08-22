e185a4183ec2ceed6ef449c7532f975b51e01601
TeamSpen210 <spencerb21@live.com>
2016-08-22 17:20:52 +1000

    Add VBSP_options module

---
+++ src/vbsp_options.py
@@ -0,0 +1,325 @@
+"""Manages reading general options from vbsp_config."""
+from collections import namedtuple
+from enum import Enum
+
+from srctools import Vec
+
+
+class TYPE(Enum):
+    """The types arguments can have."""
+    STR = str
+    INT = int
+    BOOL = bool
+    VEC = Vec
+
+# If the default is TYPE.x, the value is of that type, but default=None.
+# otherwise type = type(default).
+Opt = namedtuple('Opt', 'id default doc')
+
+
+
+DEFAULTS = [
+    Opt('goo_mist', False,
+        """Add misty info_particle_systems to goo pits.
+
+        This appears most commonly in Portal 1 maps.
+        It can be disabled globally by a style var.
+        """),
+    Opt('remove_info_lighting', False,
+        """Remove the glass/grating info_lighting entities.
+        This should be used when the border is made of brushes.
+        """),
+    Opt('remove_pedestal_plat', False,
+        """Remove the PeTI-generated pedestal button platforms.
+        """),
+    Opt('remove_exit_signs', False,
+        """Remove the exit sign overlays for singleplayer.
+        """),
+
+    Opt('rotate_edge', False,
+        """Rotate squarebeams textures 90 degrees.
+        """),
+    Opt('reset_edge_off', False,
+        """Set the offset of squarebeams to 0.
+        """),
+    Opt('edge_scale', 0.15,
+       """The scale on squarebeams textures.
+       """),
+    Opt('rotate_edge_special', TYPE.BOOL,
+        """Rotate squarebeams textures on angled/flip panels 90 degrees.
+
+        If unset, the default is read from `rotate_edge`.
+        """),
+    Opt('reset_edge_off_special', TYPE.BOOL,
+        """Set the offset of squarebeams on angled/flip panels to 0.
+
+        If unset, the default is read from `reset_edge_off`.
+        """),
+    Opt('edge_scale_special', TYPE.FLOAT,
+        """The scale on angled/flip panel squarebeams textures.
+
+        If unset, the default is read from `reset_edge_scale_special`.
+        """),
+
+    Opt('tile_texture_lock', True,
+        """If disabled, reset offsets for all white/black brushes.
+
+        This makes EmbedFace textures contiguous, for irregular textures.
+        """),
+
+    Opt('fizz_border_vertical', False,
+        """For fizzler borders, indicate that the texture is vertical.
+        """),
+
+    Opt('fizz_border_thickness', 8,
+        """For fizzler borders, set the width of the generated overlays.
+        """),
+    Opt('fizz_border_repeat', 128,
+        """For fizzler borders, the distance before the
+        texture will repeat again.
+        """,
+
+    Opt('fizz_visibility', True,
+        """Whether fizzlers should be visible.
+
+        When `False`, this disables fizzler sounds and implies
+        `fizz_scanline` is also `False`.
+        """),
+
+    Opt('force_fizz_reflect', False,
+        """Force fast reflections on fizzlers.
+        """),
+    Opt('force_brush_reflect', False,
+        """Force fast reflections on func_brushes.
+        """),
+
+    Opt('flip_sound_start', "World.a3JumpIntroRotatingPanelTravel",
+        """Set the starting sound for Flip Panel brushes.
+        """),
+    Opt('flip_sound_stop', "World.a3JumpIntroRotatingPanelArrive",
+        """Set the stopping sound for Flip Panel brushes.
+        """),
+
+    Opt('staticPan', TYPE.STR,
+        """Folder for static panels.
+
+        Instances follow the pattern `ang_type.vmf`,
+        where ang = (`00`, `30`, `45`, `60`, `90`) and type=(`glass`, `surf`).
+        """),
+    Opt('static_pan_temp_flat', "BEE2_STATIC_PAN_FLAT",
+        """Template used for 0-degree static panels.
+
+        This should be a panel sticking slightly out of the surface.
+        """),
+    Opt('static_pan_temp_white', "BEE2_STATIC_PAN_ANGLED",
+        """Template used for angled portalable static panels.
+        """),
+    Opt('static_pan_temp_black', "BEE2_STATIC_PAN_ANGLED",
+        """Template used for angled non-portalable static panels.
+        """),
+    # If set,
+    Opt('dynamic_pan_temp', TYPE.STR,
+        """If set, replace panel func_brushes with this.
+
+        The top texture should be set to `black_wall_metal_002c`.
+        """),
+    Opt('dynamic_pan_parent', "model_arms,panel_attach",
+        """The local name that the panel func_brush should parent to.
+        Adding the attachment name to the parent after a comma
+        automatically sets the attachment point for the brush.
+        """),
+
+    Opt('signInst', TYPE.STR,
+        """Adds this instance on all the signs.
+
+        The origin is positioned on the surface.
+        """),
+    Opt('signSize', 32,
+        """Set the size of the sign overlays.
+        """),
+    Opt('signPack', TYPE.STR,
+        """Packlist to use when `signInst` is added.
+        """),
+
+    Opt('broken_antline_chance', 0.0,
+        """The chance an antline will be 'broken'.
+
+        For each antline, this is checked. If true, `broken_antline_distance`
+        at most become broken.
+        """),
+    Opt('broken_antline_distance', 3,
+        """The maximum distance of a single broken section.
+        """),
+    Opt('goo_scale', 1.00,
+        """Scale of the goo textures.
+        """),
+
+    Opt('pit_blend_light', TYPE.Vec,
+        """Color of lights in bottomless pits.
+
+        These are added at in the skybox and in the map, to blend together the
+        lighting. It should be set to the ambient light color.
+        """),
+
+    Opt('glass_clip', "BEE2/glass_player_clip",
+        """Texture used for glass player clips.
+        """),
+    Opt('grating_clip', "BEE2/grate_player_clip",
+        """Texture used for grating player clips.
+        """),
+
+    # Packlists for glass and gratings
+    Opt('glass_pack', "PACK_PLAYER_CLIP_GLASS",
+        """Packlist for glass clips.
+
+        This is used for `glass_clip`.
+        """),
+    Opt('grating_pack', "PACK_PLAYER_CLIP_GRATE",
+        """Packlist for grating clips.
+
+        This is used for `grating_clip`.
+        """),
+    Opt('grating_filter', "@not_paint_bomb",
+        """Filter used on grating `func_vphysics_clips`.
+
+        """),
+    Opt('glass_template', TYPE.STR,
+        """A template for rotation and scaling of glass.
+
+        This overrides `glass_scale` if set. It should be a single brush
+        cube - the glass is set to the same rotation as the matching side.
+        """),
+    Opt('grating_template', TYPE.STR,
+        """A template for rotation and scaling of grates.
+
+        This overrides `grating_scale` if set. It should be a single brush
+        cube - the grate is set to the same rotation as the matching side.
+        """),
+
+    Opt('goo_wall_scale_temp', TYPE.STR,
+        """A template for rotation and scaling for `goo_wall` textures.
+
+        It should be a single brush cube - the wall is set to the same
+        rotation as the matching side. (The bottom is ignored).
+        """),
+
+    Opt('glass_scale', 0.15,
+        """Scale of glass textures.
+
+        This is overridden by `glass_template`.
+        """),
+    Opt('grating_scale', 0.15,
+        """Scale of grating textures.
+
+        This is overridden by `grating_template`.
+        """),
+
+    Opt('glass_floorbeam_temp', TYPE.STR,
+        """Template for beams in the middle of large glass floors.
+
+        The template should be two brushes for the texture scaling.
+        """),
+    Opt('glass_floorbeam_sep', 2,
+        """Number of blocks between beams.
+        """),
+
+    Opt('clump_wall_tex', False,
+        """Use the clumping wall algorithm.
+
+        This creates groups of the same texture.
+        `clump_size`, `clump_width`, and `clump_number` must be set.
+        """),
+    Opt('clump_size', 4,
+        """The maximum length of a clump.
+
+        Clumps are rectangular, and this indicates the long dimension.
+        """),
+    Opt('clump_width', 2,
+        """The maximum width of a clump.
+
+        Clumps are rectangular, and this indicates the short dimensions.
+        """),
+    Opt('clump_number', 6,
+        """The amount of clumps created.
+
+        The actual number of clumps is equal to
+        `surfaces / clump_max_area * clump_number`.
+        """),
+    Opt('clump_ceil', False,
+        """Apply clumping to ceilings as well.
+        """),
+    Opt('clump_floor', False,
+        """Apply clumping to floors as well.
+        """),
+
+    # Default to the origin of the elevator instance - that's likely to
+    # be enclosed
+    Opt('music_location_sp', Vec(-2000, 2000, 0),
+        """The location of music entities in SP.
+
+        The default is the location of the entry elevator instance.
+        """),
+    Opt('music_location_sp', Vec(-2000, -2000, 0),
+        """The location of music entities in Coop.
+
+        The default is the location of the disassembly room instance.
+        """),
+    # Instance used for pti_ents
+    Opt('global_pti_ents', "instances/BEE2/global_pti_ents.vmf",
+        """The instance used for global_pti_ents.
+
+        This shouldn't need to be changed.
+        """),
+    #
+    Opt('global_pti_ents_loc', Vec(-2400, -2800, 0),
+        """Location of global_pti_ents.
+
+        The default pos is next to `arrival_departure_ents`.
+        Note that many other entities are added at this point, since it's
+        sealed from the void.
+        """),
+
+    Opt('model_changer_loc', Vec(-2400, -2800, -256),
+        """Location of the model changer instance (if used).
+        """),
+
+    ######
+    # The following are set by the BEE2.4 app automatically:
+
+    Opt('bee2_loc', TYPE.STR,
+        """(Automatic) The location of the BEE2 application.
+        """),
+    Opt('game_id', "620",
+        """(Automatic) The game's steam ID.
+        """),
+    Opt('music_id', "<NONE>",
+        """The ID of the selected music.
+        """),
+    Opt('music_instance', TYPE.STR,
+        """The instance for the chosen music.
+        """),
+    Opt('music_soundscript', TYPE.STR,
+        """The soundscript for the chosen music.
+        """),
+    Opt('music_looplen', 0,
+        """If set, re-trigger music after this number of seconds.
+        """),
+    Opt('elev_type', "RAND",
+        """What type of elevator script to use:
+
+        This should be set to one of `RAND`, `FORCE`, `NONE` or `BSOD`
+        """),
+    Opt('elev_horiz', TYPE.STR,
+        """The horizontal elevator video to use.
+        """),
+    Opt('elev_vert', TYPE.STR,
+        """The vertical elevator video to use.
+        """),
+    Opt('voice_id', "<NONE>",
+        """The ID of the selected voice pack.
+        """),
+    Opt('voice_char', TYPE.STR,
+        """Comma-separated list of characters in the pack.
+        """),
+    "cave_port_skin":           "",  # If a Cave map, indicate which portrait to use.
+]
