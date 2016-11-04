94c4c62b3abb80ef87831659ff69cd8759ba0c9d
TeamSpen210 <spencerb21@live.com>
2016-11-04 13:24:36 +1000

    Generate ConnectionPoints data

--- src/editoritems_gen.py
+++ src/editoritems_gen.py
@@ -2,21 +2,61 @@
 of editoritems.
 """
 import sys
-import webbrowser
 
-from srctools import Property, Vec, VMF, Entity
+from srctools import Property, Vec, VMF
 import utils
 
 LOGGER = utils.init_logging('../logs/editoritems_gen.log')
 
+CONNECTION_OFFSETS = {
+    # Skin -> antline offset.
+    '1': Vec(-0.5, +0.5),
+    '2': Vec(-0.5, -0.5),
+    '3': Vec(+0.5, +0.5),
+    '4': Vec(+0.5, -0.5),
+}
+
 
 def make_connections(props, vmf: VMF):
-    embed_voxel = Property('ConnectionPoints', [])
+    """Generate ConnectionPoints blocks, the data for antlines."""
+    # Note - ConnectionPoints has an inverted Y compared to the game.
+
+    conn_points = Property('ConnectionPoints', [])
+    for ent in vmf.by_class['bee2_editor_connectionpoint']:
+        prop = Property('Point', [])
+        conn_points.append(prop)
+        norm = Vec(x=1).rotate_by_str(ent['angles'])
+        norm.y = - norm.y
+        prop['Dir'] = str(norm)
+
+        origin = (Vec.from_str(ent['origin']) - (-64, 64, 0)) // 16 + Vec(-0.5, 0.5, 0)
+        origin.z = 0
+        origin.y = -origin.y
+
+        try:
+            offset = CONNECTION_OFFSETS[ent['skin', '1']].copy()
+        except KeyError:
+            offset = CONNECTION_OFFSETS['1'].copy()
+        offset.rotate_by_str(ent['angles'])
+
+        offset.z = 0
+        offset.y = -offset.y
+
+        prop['Pos'] = str(origin + offset)
+        prop['SignageOffset'] = str(origin - offset)
+
+        prop['Priority'] = ent['priority', '0']
+        group_id = ent['group_id']
+        if group_id:
+            prop['GroupID'] = group_id
+
+    if conn_points:
+        props.append(conn_points)
 
 
 def make_embeddedvoxel(props, vmf: VMF):
+    """Generate the embeddedvoxel blocks."""
     embed_voxel = Property('EmbeddedVoxels', [])
-
 
 
 def main(filename):
@@ -27,7 +67,7 @@ def main(filename):
     exp_props = Property('Exporting', [])
     props = Property('Item', [exp_props])
 
-    for name, func in globals():
+    for name, func in globals().items():
         if name.startswith('make_'):
             LOGGER.info('Generating "{}"', name[5:])
             func(exp_props, vmf)
