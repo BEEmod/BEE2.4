b68a0a50730f78e8e1b7607a0ec36824862f26b4
TeamSpen210 <spencerb21@live.com>
2016-12-26 13:48:55 +1000

    Add attrs for indicators

--- src/connections.py
+++ src/connections.py
@@ -2,13 +2,16 @@
 from enum import Enum
 from srctools import Entity
 
+from typing import Iterable
+
 
 class ConnType(Enum):
     """Kind of Input A/B type, or TBeam type."""
     DEFAULT = 0  # Normal / unconfigured input
+    # Becomes one of the others based on item preference.
 
-    PRIMARY = TBEAM_IO = 1
-    SECONDARY = TBEAM_DIR = 2
+    PRIMARY = TBEAM_IO = 1  # A Type, 'normal'
+    SECONDARY = TBEAM_DIR = 2  # B Type, 'alt'
 
     BOTH = 3  # Trigger both simultaneously.
 
@@ -21,9 +24,11 @@ class Connection:
         in_inst: Entity,
         out_inst: Entity,
         conn_type=ConnType.DEFAULT,
-
+        toggle: Entity=None,
+        panels: Iterable[Entity]=(),
     ):
         self.in_inst = in_inst
         self.out_inst = out_inst
         self.type = conn_type
-
+        self.ind_panels = list(panels)
+        self.ind_toggle = toggle
