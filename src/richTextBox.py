0f58212a45eef655bfa33abcaefbede3e395fb7a
TeamSpen210 <spencerb21@live.com>
2017-01-04 10:50:58 +1000

    Add heading support to richTextBox

--- src/richTextBox.py
+++ src/richTextBox.py
@@ -30,6 +30,18 @@ class tkRichText(tkinter.Text):
             wrap="word",
             font=self.font,
         )
+
+        self.heading_font = {}
+        cur_size = self.font['size']
+        for size in range(6, 0, -1):
+            self.heading_font[size] = font = self.font.copy()
+            cur_size /= 0.8735
+            font.configure(weight='bold', size=round(cur_size))
+            self.tag_config(
+                'heading_{}'.format(size),
+                font=font,
+            )
+
         self.tag_config(
             "underline",
             underline=1,
