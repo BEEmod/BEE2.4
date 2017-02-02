b35c17f75279bc2f0914e5864784b12474fac09b
TeamSpen210 <spencerb21@live.com>
2017-02-02 13:54:27 +1000

    Pack if no packlists, but files to pack

--- src/vbsp.py
+++ src/vbsp.py
@@ -3164,7 +3164,6 @@ def make_packlist(map_path):
                 if not pack_triggers:
                     break  # No more left
 
-    if not TO_PACK:
         # Nothing to pack - wipe the packfile!
         open(map_path[:-4] + '.filelist.txt', 'w').close()
 
