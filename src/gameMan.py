6064b380985b261e016c5f09ed5e03e6d10ccc24
TeamSpen210 <spencerb21@live.com>
2017-02-06 19:19:45 +1000

    Fix funnel special inputs not being read in.

--- src/gameMan.py
+++ src/gameMan.py
@@ -678,10 +678,10 @@ class Game:
 
             # The funnel item type is special, having the additional input type.
             # Handle that specially.
-            if item['type'] == 'item_tbeam':
+            if item['type'].casefold() == 'item_tbeam':
                 for block in item.find_all('Exporting', 'Inputs', CONN_FUNNEL):
                     for io_prop in block:
-                        comm_block['TBEAM_' + io_prop.real_name] = io_prop.value
+                        comm_block['TBeam' + io_prop.real_name] = io_prop.value
 
             # Fizzlers don't work correctly with outputs. This is a signal to
             # conditions.fizzler, but it must be removed in editoritems.
