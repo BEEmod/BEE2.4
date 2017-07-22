68916882f5922c0b6f1fd8888c0b2ed9ab8e1e66
TeamSpen210 <spencerb21@live.com>
2017-07-22 16:06:23 +1000

    Add ChangeFizzlerType result
    This converts a fizzler to a different item.

--- src/conditions/fizzler.py
+++ src/conditions/fizzler.py
@@ -1,20 +1,28 @@
 """Results for custom fizzlers."""
-import conditions
-import srctools
 import utils
-import vbsp
-import instanceLocs
-import comp_consts as const
-import template_brush
 from conditions import (
     make_result, meta_cond,
     ITEMS_WITH_CLASS, CONNECTIONS
 )
-from srctools import Vec, Property, VMF, Entity, Solid, Output
-from vbsp import TEX_FIZZLER
-
-from typing import List, Dict
+from srctools import Property, Entity
+import fizzler
 
 COND_MOD_NAME = 'Fizzlers'
 
 LOGGER = utils.getLogger(__name__, alias='cond.fizzler')
+
+
+@make_result('ChangeFizzlerType')
+def res_change_fizzler_type(inst: Entity, res: Property):
+    """Change the type of a fizzler. Only valid when run on the base instance."""
+    fizz_name = inst['targetname']
+    try:
+        fizz = fizzler.FIZZLERS[fizz_name]
+    except KeyError:
+        LOGGER.warning('ChangeFizzlerType not run on a fizzler ("{}")!', fizz_name)
+        return
+
+    try:
+        fizz.fizz_type = fizzler.FIZZ_TYPES[res.value]
+    except KeyError:
+        raise ValueError('Invalid fizzler type "{}"!', res.value)
