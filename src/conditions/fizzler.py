c27a00b60fc2dd65b6bdd027fe21b4ac5474d34b
TeamSpen210 <spencerb21@live.com>
2017-07-22 17:45:26 +1000

    Initial reshaping fizzler work
    * Works, except for antlines and connections -> Need connections upgrade.

--- src/conditions/fizzler.py
+++ src/conditions/fizzler.py
@@ -1,10 +1,8 @@
 """Results for custom fizzlers."""
 import utils
-from conditions import (
-    make_result, meta_cond,
-    ITEMS_WITH_CLASS, CONNECTIONS
-)
-from srctools import Property, Entity
+from conditions import make_result, remove_ant_toggle
+import connections
+from srctools import Property, Entity, Vec
 import fizzler
 
 COND_MOD_NAME = 'Fizzlers'
@@ -26,3 +24,50 @@ def res_change_fizzler_type(inst: Entity, res: Property):
         fizz.fizz_type = fizzler.FIZZ_TYPES[res.value]
     except KeyError:
         raise ValueError('Invalid fizzler type "{}"!', res.value)
+
+
+@make_result('ReshapeFizzler')
+def res_reshape_fizzler(shape_inst: Entity, res: Property):
+    """Convert a fizzler connected via the output to a new shape.
+
+    This allows for different placing of fizzler items.
+    Each 'segment' parameter should be a 'x y z;x y z' pair of positions
+    that represent the ends of the fizzler.
+    'up_axis' should be set to a normal vector pointing in the new 'upward'
+    direction.
+    """
+    shape_item = connections.ITEMS[shape_inst['targetname']]
+
+    for conn in shape_item.outputs:
+        fizz_name = conn.inp.name
+        try:
+            fizz = fizzler.FIZZLERS[fizz_name]
+        except KeyError:
+            LOGGER.warning('Reshaping fizzler with non-fizzler output! Ignoring!')
+            continue
+        break
+    else:
+        LOGGER.warning('Reshaping fizzler without a fizzler!')
+        return
+
+    # Detach this connection and remove traces of it.
+    conn.remove()
+    if shape_item.ind_toggle:
+        remove_ant_toggle(shape_item.ind_toggle)
+
+    fizz_base = fizz.base_inst
+    fizz_base['origin'] = shape_inst['origin']
+    origin = Vec.from_str(shape_inst['origin'])
+
+    shape_angles = Vec.from_str(shape_inst['angles'])
+
+    fizz.up_axis = res.vec('up_axis').rotate(*shape_angles)
+    fizz.emitters.clear()
+
+    for seg_prop in res.find_all('Segment'):
+        vec1, vec2 = seg_prop.value.split(';')
+        seg_min_max = Vec.bbox(
+            Vec.from_str(vec1).rotate(*shape_angles) + origin,
+            Vec.from_str(vec2).rotate(*shape_angles) + origin,
+        )
+        fizz.emitters.append(seg_min_max)
