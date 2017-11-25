157a0f65cd27aad80b33fdb700a83245a1e490aa
TeamSpen210 <spencerb21@live.com>
2017-11-25 18:28:24 +1000

    Rewrite fizzler reshaping with connections

--- src/conditions/fizzler.py
+++ src/conditions/fizzler.py
@@ -1,6 +1,7 @@
 """Results for custom fizzlers."""
 from conditions import make_result, make_flag
 from srctools import Property, Entity, Vec, VMF
+from instanceLocs import resolve as resolve_inst
 import connections
 import fizzler
 import utils
@@ -51,49 +52,46 @@ def res_reshape_fizzler(vmf: VMF, shape_inst: Entity, res: Property):
     shape_name = shape_inst['targetname']
     shape_item = connections.ITEMS.pop(shape_name)
 
+    shape_angles = Vec.from_str(shape_inst['angles'])
+    up_axis = res.vec('up_axis').rotate(*shape_angles)
+
     for conn in shape_item.outputs:
-        fizz_item = conn.from_item
+        fizz_item = conn.to_item
         try:
             fizz = fizzler.FIZZLERS[fizz_item.name]
         except KeyError:
-            LOGGER.warning('Reshaping fizzler with non-fizzler output! Ignoring!')
+            LOGGER.warning('Reshaping fizzler with non-fizzler output ({})! Ignoring!', fizz_item.name)
             continue
+        fizz.emitters.clear()  # Remove old positions.
+        fizz.up_axis = up_axis
         break
     else:
         # No fizzler, so generate a default.
-        # We create the fizzer instance, Fizzler object, and Item object
+        # We create the fizzler instance, Fizzler object, and Item object
         # matching it.
+        # This is hardcoded to use regular Emancipation Fields.
         conn = None
-        try:
-            fizz_type = fizzler.FIZZ_TYPES[res['default']]
-        except KeyError:
-            LOGGER.error(
-                'Fizzler shape needs a valid default '
-                'item type, not "{}"!',
-                res['default']
-            )
-            return
         base_inst = vmf.create_ent(
             targetname=shape_name,
             classname='func_instance',
             origin=shape_inst['origin'],
-            file=fizz_type.inst[fizzler.FizzInst.BASE][0],
+            file=resolve_inst('<ITEM_BARRIER_HAZARD:fizz_base>'),
         )
         base_inst.fixup.update(shape_inst.fixup)
         fizz = fizzler.FIZZLERS[shape_name] = fizzler.Fizzler(
-            fizz_type,
-            Vec(),
+            fizzler.FIZZ_TYPES['VALVE_MATERIAL_EMANCIPATION_GRID'],
+            up_axis,
             base_inst,
             [],
         )
         fizz_item = connections.Item(
             base_inst,
-            connections.ITEM_TYPES.get(res['default'], None),
+            connections.ITEM_TYPES['item_barrier_hazard'],
         )
         connections.ITEMS[shape_name] = fizz_item
 
     # Detach this connection and remove traces of it.
-    if conn:
+    for conn in list(shape_item.outputs):
         conn.remove()
     for coll in [shape_item.antlines, shape_item.ind_panels, shape_item.shape_signs]:
         for ent in coll:
@@ -106,11 +104,6 @@ def res_reshape_fizzler(vmf: VMF, shape_inst: Entity, res: Property):
     fizz_base = fizz.base_inst
     fizz_base['origin'] = shape_inst['origin']
     origin = Vec.from_str(shape_inst['origin'])
-
-    shape_angles = Vec.from_str(shape_inst['angles'])
-
-    fizz.up_axis = res.vec('up_axis').rotate(*shape_angles)
-    fizz.emitters.clear()
 
     for seg_prop in res.find_all('Segment'):
         vec1, vec2 = seg_prop.value.split(';')
