23033bd98bd299d98d2945d11cf29960bcbc1795
TeamSpen210 <spencerb21@live.com>
2017-11-29 16:30:37 +1000

    Rewrite Resizeable Triggers for connections

--- src/conditions/resizableTrigger.py
+++ src/conditions/resizableTrigger.py
@@ -1,15 +1,18 @@
 """Logic for trigger items, allowing them to be resized."""
+from contextlib import suppress
+
 import conditions
 import instanceLocs
-import srctools
 import utils
 import vbsp
 import vbsp_options
 import comp_consts as const
+import connections
 from conditions import (
     make_result, RES_EXHAUSTED,
 )
-from srctools import Property, Vec, Entity, Output
+from srctools import Property, Vec, Output, VMF
+
 
 COND_MOD_NAME = None
 
@@ -17,7 +20,7 @@ LOGGER = utils.getLogger(__name__, alias='cond.resizeTrig')
 
 
 @make_result('ResizeableTrigger')
-def res_resizeable_trigger(res: Property):
+def res_resizeable_trigger(vmf: VMF, res: Property):
     """Replace two markers with a trigger brush.  
 
     This is run once to affect all of an item.  
@@ -25,23 +28,23 @@ def res_resizeable_trigger(res: Property):
     * `markerInst`: <ITEM_ID:1,2> value referencing the marker instances, or a filename.
     * `markerItem`: The item's ID
     * `previewConf`: A item config which enables/disables the preview overlay.
-    * `previewinst`: An instance to place at the marker location in preview mode.
+    * `previewInst`: An instance to place at the marker location in preview mode.
         This should contain checkmarks to display the value when testing.
     * `previewMat`: If set, the material to use for an overlay func_brush.
         The brush will be parented to the trigger, so it vanishes once killed.
         It is also non-solid.
     * `previewScale`: The scale for the func_brush materials.
-    * `previewActivate`, `previewDeactivate`: The `instance:name;Input` value
-        to turn the previewInst on and off.
+    * `previewActivate`, `previewDeactivate`: The VMF output to turn the
+        previewInst on and off.
 
-    * `triggerActivate, triggerDeactivate`: The outputs used when the trigger
-        turns on or off.
+    * `triggerActivate, triggerDeactivate`: The `instance:name;Output`
+        outputs used when the trigger turns on or off.
 
     * `coopVar`: The instance variable which enables detecting both Coop players.
         The trigger will be a trigger_playerteam.
 
-    * `coopActivate, coopDeactivate`: The outputs used when coopVar is enabled.
-        These should be suitable for a logic_coop_manager.
+    * `coopActivate, coopDeactivate`: The `instance:name;Output` outputs used
+        when coopVar is enabled. These should be suitable for a logic_coop_manager.
     * `coopOnce`: If true, kill the manager after it first activates.
 
     * `keys`: A block of keyvalues for the trigger brush. Origin and targetname
@@ -51,88 +54,114 @@ def res_resizeable_trigger(res: Property):
     """
     marker = instanceLocs.resolve(res['markerInst'])
 
-    markers = {}
-    for inst in vbsp.VMF.by_class['func_instance']:
-        if inst['file'].casefold() in marker:
-            markers[inst['targetname']] = inst
+    marker_names = set()
 
-    if not markers:  # No markers in the map - abort
+    for inst in vmf.by_class['func_instance']:
+        if inst['file'].casefold() in marker:
+            marker_names.add(inst['targetname'])
+            # Unconditionally delete from the map, so it doesn't
+            # appear even if placed wrongly.
+            inst.remove()
+
+    if not marker_names:  # No markers in the map - abort
         return RES_EXHAUSTED
 
-    trig_act = res['triggerActivate', 'OnStartTouchAll']
-    trig_deact = res['triggerDeactivate','OnEndTouchAll']
+    item_id = res['markerItem']
 
-    coop_var = res['coopVar', None]
-    coop_act = res['coopActivate', 'OnChangeToAllTrue']
-    coop_deact = res['coopDeactivate', 'OnChangeToAnyFalse']
-    coop_only_once = res.bool('coopOnce')
+    # Synthesise the item type used for the final trigger.
+    item_type_sp = connections.ItemType(
+        id=item_id + ':TRIGGER',
+        output_act=Output.parse_name(res['triggerActivate', 'OnStartTouchAll']),
+        output_deact=Output.parse_name(res['triggerDeactivate', 'OnEndTouchAll']),
+    )
 
-    marker_connection = conditions.CONNECTIONS[res['markerItem'].casefold()]
-    mark_act_name, mark_act_out = marker_connection.out_act
-    mark_deact_name, mark_deact_out = marker_connection.out_deact
-    del marker_connection
+    # For Coop, we add a logic_coop_manager in the mix so both players can
+    # be handled.
+    try:
+        coop_var = res['coopVar']
+    except LookupError:
+        coop_var = item_type_coop = None
+        coop_only_once = False
+    else:
+        coop_only_once = res.bool('coopOnce')
+        item_type_coop = connections.ItemType(
+            id=item_id + ':TRIGGER_COOP',
+            output_act=Output.parse_name(
+                res['coopActivate', 'OnChangeToAllTrue']
+            ),
+            output_deact=Output.parse_name(
+                res['coopDeactivate', 'OnChangeToAnyFalse']
+            ),
+        )
 
     # Display preview overlays if it's preview mode, and the config is true
+    pre_act = pre_deact = None
     if vbsp.IS_PREVIEW and vbsp_options.get_itemconf(res['previewConf', ''], False):
         preview_mat = res['previewMat', '']
         preview_inst_file = res['previewInst', '']
-        pre_act_name, pre_act_inp = Output.parse_name(
-            res['previewActivate', ''])
-        pre_deact_name, pre_deact_inp = Output.parse_name(
-            res['previewDeactivate', ''])
-        preview_scale = srctools.conv_float(res['previewScale', '0.25'], 0.25)
+        preview_scale = res.float('previewScale', 0.25)
+        # None if not found.
+        with suppress(LookupError):
+            pre_act = Output.parse(res.find_key('previewActivate'))
+        with suppress(LookupError):
+            pre_deact = Output.parse(res.find_key('previewDeactivate'))
     else:
         # Deactivate the preview_ options when publishing.
         preview_mat = preview_inst_file = ''
-        pre_act_name = pre_deact_name = None
-        pre_act_inp = pre_deact_inp = ''
         preview_scale = 0.25
 
-    # Now convert each brush
-    # Use list() to freeze it, allowing us to delete from the dict
-    for targ, inst in list(markers.items()):  # type: str, VLib.Entity
-        for out in inst.output_targets():
-            if out in markers:
-                other = markers[out]  # type: Entity
-                del markers[out]  # Don't let it get repeated
+    # Now go through each brush.
+    # We do while + pop to allow removing both names each loop through.
+    todo_names = set(marker_names)
+    while todo_names:
+        targ = todo_names.pop()
+
+        mark1 = connections.ITEMS.pop(targ)
+        for conn in mark1.outputs:
+            if conn.to_item.name in marker_names:
+                mark2 = conn.to_item
+                conn.remove()  # Delete this connection.
+                todo_names.discard(mark2.name)
+                del connections.ITEMS[mark2.name]
                 break
         else:
-            if inst.fixup['$connectioncount'] == '0':
+            if not mark1.inputs:
                 # If the item doesn't have any connections, 'connect'
                 # it to itself so we'll generate a 1-block trigger.
-                other = inst
+                mark2 = mark1
             else:
-                continue  # It's a marker with an input, the other in the pair
+                # It's a marker with an input, the other in the pair
                 # will handle everything.
+                # But reinstate it in ITEMS.
+                connections.ITEMS[targ] = mark1
+                continue
 
-        for ent in {inst, other}:
-            # Only do once if inst == other
-            ent.remove()
+        inst1 = mark1.inst
+        inst2 = mark2.inst
 
         is_coop = coop_var is not None and vbsp.GAME_MODE == 'COOP' and (
-            inst.fixup.bool(coop_var) or
-            other.fixup.bool(coop_var)
+            inst1.fixup.bool(coop_var) or
+            inst2.fixup.bool(coop_var)
         )
 
         bbox_min, bbox_max = Vec.bbox(
-            Vec.from_str(inst['origin']),
-            Vec.from_str(other['origin'])
+            Vec.from_str(inst1['origin']),
+            Vec.from_str(inst2['origin'])
         )
+        origin = (bbox_max + bbox_min) / 2
 
         # Extend to the edge of the blocks.
         bbox_min -= 64
         bbox_max += 64
 
-        out_ent = trig_ent = vbsp.VMF.create_ent(
+        out_ent = trig_ent = vmf.create_ent(
             classname='trigger_multiple',  # Default
-            # Use the 1st instance's name - that way other inputs control the
-            # trigger itself.
             targetname=targ,
-            origin=inst['origin'],
+            origin=origin,
             angles='0 0 0',
         )
         trig_ent.solids = [
-            vbsp.VMF.make_prism(
+            vmf.make_prism(
                 bbox_min,
                 bbox_max,
                 mat=const.Tools.TRIGGER,
@@ -146,35 +175,40 @@ def res_resizeable_trigger(res: Property):
             trig_ent['spawnflags'] = '1'  # Clients
             trig_ent['classname'] = 'trigger_playerteam'
 
-            out_ent_name = conditions.local_name(inst, 'man')
-            out_ent = vbsp.VMF.create_ent(
+            out_ent = manager = vmf.create_ent(
                 classname='logic_coop_manager',
-                targetname=out_ent_name,
-                origin=inst['origin']
+                targetname=conditions.local_name(inst, 'man'),
+                origin=origin,
             )
+
+            item = connections.Item(out_ent, item_type_coop)
+
             if coop_only_once:
                 # Kill all the ents when both players are present.
-                out_ent.add_out(
-                    Output('OnChangeToAllTrue', out_ent_name, 'Kill'),
+                manager.add_out(
+                    Output('OnChangeToAllTrue', manager, 'Kill'),
                     Output('OnChangeToAllTrue', targ, 'Kill'),
                 )
             trig_ent.add_out(
-                Output('OnStartTouchBluePlayer', out_ent_name, 'SetStateATrue'),
-                Output('OnStartTouchOrangePlayer', out_ent_name, 'SetStateBTrue'),
-                Output('OnEndTouchBluePlayer', out_ent_name, 'SetStateAFalse'),
-                Output('OnEndTouchOrangePlayer', out_ent_name, 'SetStateBFalse'),
+                Output('OnStartTouchBluePlayer', manager, 'SetStateATrue'),
+                Output('OnStartTouchOrangePlayer', manager, 'SetStateBTrue'),
+                Output('OnEndTouchBluePlayer', manager, 'SetStateAFalse'),
+                Output('OnEndTouchOrangePlayer', manager, 'SetStateBFalse'),
             )
-            act_out = coop_act
-            deact_out = coop_deact
         else:
-            act_out = trig_act
-            deact_out = trig_deact
+            item = connections.Item(trig_ent, item_type_sp)
+
+        # Register, and copy over all the antlines.
+        connections.ITEMS[item.name] = item
+        item.ind_panels = mark1.ind_panels | mark2.ind_panels
+        item.antlines = mark1.antlines | mark2.antlines
+        item.shape_signs = mark1.shape_signs + mark2.shape_signs
 
         if preview_mat:
-            preview_brush = vbsp.VMF.create_ent(
+            preview_brush = vmf.create_ent(
                 classname='func_brush',
                 parentname=targ,
-                origin=inst['origin'],
+                origin=origin,
 
                 Solidity='1',  # Not solid
                 drawinfastreflection='1',  # Draw in goo..
@@ -187,7 +221,7 @@ def res_resizeable_trigger(res: Property):
             )
             preview_brush.solids = [
                 # Make it slightly smaller, so it doesn't z-fight with surfaces.
-                vbsp.VMF.make_prism(
+                vmf.make_prism(
                     bbox_min + 0.5,
                     bbox_max - 0.5,
                     mat=preview_mat,
@@ -197,54 +231,27 @@ def res_resizeable_trigger(res: Property):
                 face.scale = preview_scale
 
         if preview_inst_file:
-            vbsp.VMF.create_ent(
+            pre_inst = vmf.create_ent(
                 classname='func_instance',
                 targetname=targ + '_preview',
                 file=preview_inst_file,
                 # Put it at the second marker, since that's usually
                 # closest to antlines if present.
-                origin=other['origin'],
+                origin=inst2['origin'],
             )
 
-            if pre_act_name and trig_act:
-                out_ent.add_out(Output(
-                    trig_act,
-                    targ + '_preview',
-                    inst_in=pre_act_name,
-                    inp=pre_act_inp,
-                ))
-            if pre_deact_name and trig_deact:
-                out_ent.add_out(Output(
-                    trig_deact,
-                    targ + '_preview',
-                    inst_in=pre_deact_name,
-                    inp=pre_deact_inp,
-                ))
+            if pre_act is not None:
+                out = pre_act.copy()
+                out.inst_out, out.output = item.output_act()
+                out.target = conditions.local_name(pre_inst, out.target)
+                out_ent.add_out(out)
+            if pre_deact is not None:
+                out = pre_deact.copy()
+                out.inst_out, out.output = item.output_deact()
+                out.target = conditions.local_name(pre_inst, out.target)
+                out_ent.add_out(out)
 
-        # Now copy over the outputs from the markers, making it work.
-        for out in inst.outputs + other.outputs:
-            # Skip the output joining the two markers together.
-            if out.target == other['targetname']:
-                continue
-
-            if out.inst_out == mark_act_name and out.output == mark_act_out:
-                ent_out = act_out
-            elif out.inst_out == mark_deact_name and out.output == mark_deact_out:
-                ent_out = deact_out
-            else:
-                continue  # Skip this output - it's somehow invalid for this item.
-
-            if not ent_out:
-                continue  # Allow setting the output to "" to skip
-
-            out_ent.add_out(Output(
-                ent_out,
-                out.target,
-                inst_in=out.inst_in,
-                inp=out.input,
-                param=out.params,
-                delay=out.delay,
-                times=out.times,
-            ))
+        for conn in mark1.outputs | mark2.outputs:
+            conn.from_item = item
 
     return RES_EXHAUSTED
