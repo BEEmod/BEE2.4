7300449111bf92609392b479d949505f939a1a8b
TeamSpen210 <spencerb21@live.com>
2018-12-10 15:22:32 +1000

    Locate the position of each corner item.

--- src/conditions/connections.py
+++ src/conditions/connections.py
@@ -1,18 +1,18 @@
 """Results relating to item connections."""
-import srctools.logger
+from typing import List
+
 import connections
-from conditions import (
-    make_flag, make_result, make_result_setup,
-    resolve_value, local_name,
-)
-from srctools import Property, Entity, Output
+import srctools.logger
+import conditions
+from srctools import Property, Entity, Output, VMF, Vec
+
 
 COND_MOD_NAME = 'I/O'
 
 LOGGER = srctools.logger.get_logger(__name__, alias='cond.connections')
 
 
-@make_result_setup('AddOutput')
+@conditions.make_result_setup('AddOutput')
 def res_add_output_setup(res: Property):
     output = res['output']
     input_name = res['input']
@@ -45,7 +45,7 @@ def res_add_output_setup(res: Property):
     )
 
 
-@make_result('AddOutput')
+@conditions.make_result('AddOutput')
 def res_add_output(inst: Entity, res: Property):
     """Add an output from an instance to a global or local name.
 
@@ -87,30 +87,71 @@ def res_add_output(inst: Entity, res: Property):
                 return
             inst_out, output = item_type.output_deact
     else:
-        output = resolve_value(inst, out_id)
-        inst_out = resolve_value(inst, inst_out)
+        output = conditions.resolve_value(inst, out_id)
+        inst_out = conditions.resolve_value(inst, inst_out)
 
     inst.add_out(Output(
-        resolve_value(inst, output),
-        local_name(inst, resolve_value(inst, targ)) or inst['targetname'],
-        resolve_value(inst, input_name),
-        resolve_value(inst, parm),
-        srctools.conv_float(resolve_value(inst, delay)),
+        conditions.resolve_value(inst, output),
+        conditions.local_name(inst, conditions.resolve_value(inst, targ)) or inst['targetname'],
+        conditions.resolve_value(inst, input_name),
+        conditions.resolve_value(inst, parm),
+        srctools.conv_float(conditions.resolve_value(inst, delay)),
         times=times,
-        inst_out=resolve_value(inst, inst_out) or None,
-        inst_in=resolve_value(inst, inst_in) or None,
+        inst_out=conditions.resolve_value(inst, inst_out) or None,
+        inst_in=conditions.resolve_value(inst, inst_in) or None,
     ))
 
 
-@make_result('MarkLocking')
-def res_locking_output():
+@conditions.make_result('MarkLocking')
+def res_locking_output() -> None:
     """This result is no longer used."""
     LOGGER.warning('MarkLocking is no longer used. Configure locking items in the enhanced editoritems configuration.')
 
 
-@make_flag('LockingIO')
-def res_locking_input():
+@conditions.make_flag('LockingIO')
+def res_locking_input() -> bool:
     """This flag is no longer used."""
     LOGGER.warning('LockingIO is no longer used. Configure locking items in the enhanced editoritems configuration.')
     return False
+
+
+CORNER_POS = [
+    Vec(8, 56, -64),
+    Vec(8, 40, -64),
+    Vec(8, 24, -64),
+    Vec(8, 8, -64),
+    Vec(-8, 56, -64),
+    Vec(-8, 40, -64),
+    Vec(-8, 24, -64),
+    Vec(-8, 8, -64),
+]
+
+
+class Corner:
+    """Antline Corner item."""
+    def __init__(self, item: connections.Item, pos: Vec):
+        self.item = item
+        self.pos = pos
+
+
+@conditions.meta_cond(-900)
+def antline_layouter(vmf: VMF) -> None:
+    """Implements the Antline Corner item."""
+    try:
+        corner_type = connections.ITEM_TYPES['item_bee2_antline_corner']
+    except KeyError:
+        # Item not installed.
+        return
+
+    corners = []  # type: List[Corner]
+
+    for item in connections.ITEMS.values():
+        if item.item_type is corner_type:
+            timer_delay = item.inst.fixup.int('$timer_delay')
+            pos = CORNER_POS[max(0, timer_delay - 3) % 8].copy()
+            pos.localise(
+                Vec.from_str(item.inst['origin']),
+                Vec.from_str(item.inst['angles']),
+            )
+            corners.append(Corner(item, pos))
 
