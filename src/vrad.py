04c4a3bfe23065173ff956211c685eaf31845407
TeamSpen210 <spencerb21@live.com>
2019-08-20 18:21:16 +1000

    Rewrite VRAD music stack generation using sndscript module
    Also switch syncing to use opvar-based tracker.

--- src/vrad.py
+++ src/vrad.py
@@ -27,6 +27,7 @@ from srctools.filesys import (
 from srctools.packlist import PackList, FileType as PackType, load_fgd
 from srctools.game import find_gameinfo
 from srctools.bsp_transform import run_transformations
+from srctools import sndscript as snd
 
 
 CONF = Property('Config', [])
@@ -53,6 +54,9 @@ SOUND_MAN_FOLDER = {
     utils.STEAM_IDS['TWTM']: 'twtm',
     utils.STEAM_IDS['APTAG']: 'aperturetag',
 }
+
+# Opvar name used to sync tracks.
+MUSIC_SYNC_OPVAR = 'bee2_music_start'
 
 # Files that VBSP may generate, that we want to insert into the packfile.
 # They are all found in bee2/inject/.
@@ -91,225 +95,6 @@ MDL_ADDITIONAL_EXT = [
     '.phy',
 ]
 
-
-# Various parts of the soundscript generated for BG music.
-
-# Things that can appear at the beginning of filenames..
-SOUND_CHARS = '*#@><^)}$!?'
-
-# The starting section defining the name and volume.
-# SNDLVL_NONE means it's infinite range.
-MUSIC_START = """\
-"music.BEE2{name}"
-{{
-"channel" "CHAN_STATIC"
-"soundlevel" "SNDLVL_NONE"
-"volume" "{vol}"
-"""
-
-# The basic operator stack for music without any additional tracks.
-MUSIC_BASE = """\
-"soundentry_version" "2"
-"operator_stacks"
-\t{
-\t"update_stack"
-\t\t{
-\t\t"import_stack" "update_music_stereo"
-\t\t"volume_fade_in"
-\t\t\t{
-\t\t\t"input_max" "0.5"
-\t\t\t}
-\t\t"volume_fade_out"
-\t\t\t{
-\t\t\t"input_max" "1.5"
-\t\t\t}
-"""
-
-# We need to stop the sub-tracks after the main track stops...
-MUSIC_END = """\
-\t\t}
-\t"stop_stack"
-\t\t{
-\t\t"stop_entry"
-\t\t\t{
-\t\t\t"operator" "sys_stop_entries"
-\t\t\t"input_max_entries" "0"
-\t\t\t"match_entity" "false"
-\t\t\t"match_substring" "true"
-\t\t\t"match_entry" "music.BEE2_"
-\t\t\t}
-\t\t}
-\t}
-}
-"""
-
-# Operator stacks which enable the given gel types.
-MUSIC_GEL_BOUNCE_MAIN = """\
-
-\t\t"import_stack" "p2_update_music_play_gel"
-\t\t"gel_play_entry"
-\t\t\t{
-\t\t\t"entry_name" "music.BEE2_gel_bounce"
-\t\t\t}
-\t\t"gel_stop_entry"
-\t\t\t{
-\t\t\t"match_entry" "music.BEE2_gel_bounce"
-\t\t\t}
-"""
-
-MUSIC_GEL_SPEED_MAIN = """\
-
-\t\t"import_stack" "p2_update_music_play_speed_gel"
-\t\t"speed_velocity_trigger"
-\t\t\t{
-\t\t\t"input2" "250"
-\t\t\t}
-\t\t"speed_play_entry"
-\t\t\t{
-\t\t\t"entry_name" "music.BEE2_gel_speed"
-\t\t\t}
-\t\t"speed_stop_entry"
-\t\t\t{
-\t\t\t"match_entry" "music.BEE2_gel_speed"
-\t\t\t}
-"""
-
-MUSIC_FUNNEL_MAIN = """\
-
-\t"import_stack" "p2_update_music_play_tbeam"
-\t"play_entry"
-\t\t{
-\t\t"entry_name" "music.BEE2_funnel"
-\t\t}
-\t"stop_entry"
-\t\t{
-\t\t"match_entry" "music.BEE2_funnel"
-\t\t}
-"""
-
-# The gel operator stack syncronises the music with the base track.
-MUSIC_GEL_STACK = """\
-
-"soundentry_version" "2"
-"operator_stacks"
-\t{{
-\t"start_stack"
-\t\t{{
-\t\t"import_stack" "start_sync_to_entry"
-\t\t"elapsed_time"
-\t\t\t{{
-\t\t\t"entry" "music.BEE2"
-\t\t\t}}
-\t\t"duration_div"
-\t\t\t{{
-\t\t\t"input2" "1"
-\t\t\t}}
-\t\t"div_mult"
-\t\t\t{{
-\t\t\t"input1" "1.0"
-\t\t\t}}
-\t\t}}
-\t"update_stack"
-\t\t{{
-\t\t"import_stack" "update_music_stereo"
-\t\t"volume_fade_in"
-\t\t\t{{
-\t\t\t"input_max" "{fadein}"
-\t\t\t}}
-\t\t"volume_fade_out"
-\t\t\t{{
-\t\t\t"input_max" "{fadeout}"
-\t\t\t}}
-\t\t}}
-\t}}
-}}
-"""
-
-# This funnel stack makes it start randomly offset into the music.
-MUSIC_FUNNEL_RAND_STACK = """\
-
-"soundentry_version" "2"
-"operator_stacks"
-\t{
-\t"start_stack"
-\t\t{
-\t\t"random_offset"
-\t\t\t{
-\t\t\t"operator" "math_random"
-\t\t\t"input_min" "0.0"
-\t\t\t"input_max" "126"
-\t\t\t}
-\t\t"negative_delay"
-\t\t\t{
-\t\t\t"operator" "math_float"
-\t\t\t"apply" "mult"
-\t\t\t"input1" "@random_offset.output"
-\t\t\t"input2" "-1.0"
-\t\t\t}
-\t\t"delay_output"
-\t\t\t{
-\t\t\t"operator" "sys_output"
-\t\t\t"input_float" "@negative_delay.output"
-\t\t\t"output" "delay"
-\t\t\t}
-\t\t}
-"""
-
-# This funnel stack makes it synchronise with the main track.
-MUSIC_FUNNEL_SYNC_STACK = """\
-
-"soundentry_version" "2"
-"operator_stacks"
-\t{
-\t"start_stack"
-\t\t{
-\t\t"import_stack" "start_sync_to_entry"
-\t\t"elapsed_time"
-\t\t\t{
-\t\t\t"entry" "music.BEE2"
-\t\t\t}
-\t\t"duration_div"
-\t\t\t{
-\t\t\t"input2" "1"
-\t\t\t}
-\t\t"div_mult"
-\t\t\t{
-\t\t\t"input1" "1.0"
-\t\t\t}
-\t\t}
-"""
-
-# Both funnel versions share the same update stack.
-MUSIC_FUNNEL_UPDATE_STACK = """\
-\t"update_stack"
-\t\t{
-\t\t"import_stack" "update_music_stereo"
-\t\t"mixer"
-\t\t\t{
-\t\t\t"mixgroup" "unduckedMusic"
-\t\t\t}
-\t\t"volume_fade_in"
-\t\t\t{
-\t\t\t"input_max" "3.0"
-\t\t\t"input_map_min" "0.05"
-\t\t\t}
-\t\t"volume_fade_out"
-\t\t\t{
-\t\t\t"input_max" "0.75"
-\t\t\t"input_map_min" "0.05"
-\t\t\t}
-\t\t"volume_lfo_time_scale"
-\t\t\t{
-\t\t\t"input2" "0.3"
-\t\t\t}
-\t\t"volume_lfo_scale"
-\t\t\t{
-\t\t\t"input2" "0.4"
-\t\t\t}
-\t\t}
-\t}
-}
-"""
 
 
 def load_config():
@@ -378,98 +163,217 @@ def generate_music_script(data: Property, pack_list: PackList) -> bytes:
         base = data.find_key('base')
 
     # The sounds must be present, and the items should be in the map.
-    has_funnel = funnel.value and (
+    has_funnel = bool(funnel) and (
         'funnel' in voice_attr or
         'excursionfunnel' in voice_attr
     )
-    has_bounce = bounce.value and (
+    has_bounce = bool(bounce) and (
         'bouncegel' in voice_attr or
         'bluegel' in voice_attr
     )
     # Speed-gel sounds also play when flinging, so keep it always.
+    has_speed = bool(speed)
 
-    file = StringIO()
+    sound_base = snd.Sound(
+        "music.BEE2_base",
+        ['#*' + wav for wav in base.as_array()],
+        channel=snd.Channel.BACKGROUND,
+        level=snd.Level.SNDLVL_NONE,
+    )
+    sound_start = snd.Sound(
+        "music.BEE2_start",
+        ['common/null.wav'],
+    )
+    sound_stop = snd.Sound(
+        "music.BEE2_stop",
+        ['common/null.wav']
+    )
 
-    # Write the base music track
-    file.write(MUSIC_START.format(name='', vol='1'))
-    write_sound(file, base, pack_list, snd_prefix='#*')
-    file.write(MUSIC_BASE)
-    # The 'soundoperators' section is still open now.
+    sounds = [sound_start, sound_stop, sound_base]
+    sounds_synch = []
 
-    # Add the operators to play the auxilluary sounds..
+    # A pair of proxy tracks starting and stopping the music.
+    with sound_start.stack_start.build() as builder:
+        builder.import_stack("P2_null_player_start")
+        with builder.play_entry:
+            builder.entry_name("music.BEE2_base")
+
+    # We need to stop the sub-tracks after the main track stops...
+    with sound_stop.stack_start.build() as builder:
+        builder.import_stack("P2_null_player_start")
+        with builder.stop_entry:
+            builder.operator("sys_stop_entries")
+            builder.input_max_entries("0")
+            builder.match_entity("false")
+            builder.match_substring("true")
+            builder.match_entry("music.BEE2_")
+
+    with sound_base.stack_update.build() as builder:
+        builder.import_stack("update_music_stereo")
+        with builder.volume_fade_in:
+            builder.input_max("0.5")
+        with builder.volume_fade_out:
+            builder.input_max("1.5")
+
+        # Add the operators to play the auxiliary sounds..
+        if has_funnel:
+            builder.import_stack("p2_update_music_play_tbeam")
+            with builder.play_entry:
+                builder.entry_name("music.BEE2_funnel")
+            with builder.stop_entry:
+                builder.match_entry("music.BEE2_funnel")
+
+        if has_bounce:
+            builder.import_stack("p2_update_music_play_gel")
+            with builder.gel_play_entry:
+                builder.entry_name("music.BEE2_gel_bounce")
+            with builder.gel_stop_entry:
+                builder.match_entry("music.BEE2_gel_bounce")
+
+        if has_speed:
+            builder.import_stack("p2_update_music_play_speed_gel")
+            with builder.speed_velocity_trigger:
+                builder.input2("250")
+            with builder.speed_play_entry:
+                builder.entry_name("music.BEE2_gel_speed")
+            with builder.speed_stop_entry:
+                builder.match_entry("music.BEE2_gel_speed")
+
     if has_funnel:
-        file.write(MUSIC_FUNNEL_MAIN)
-    if has_bounce:
-        file.write(MUSIC_GEL_BOUNCE_MAIN)
-    if speed.value:
-        file.write(MUSIC_GEL_SPEED_MAIN)
+        sound_funnel = snd.Sound(
+            "music.BEE2_funnel",
+            ['*' + wav for wav in funnel.as_array()],
+            channel=snd.Channel.BACKGROUND,
+            level=snd.Level.SNDLVL_NONE,
+        )
+        sounds.append(sound_funnel)
 
-    # End the main sound block
-    file.write(MUSIC_END)
-
-    if has_funnel:
-        # Write the 'music.BEE2_funnel' sound entry
-        file.write('\n')
-        file.write(MUSIC_START.format(name='_funnel', vol='1'))
-        write_sound(file, funnel, pack_list, snd_prefix='*')
         # Some tracks want the funnel music to sync with the normal
         # track, others randomly choose a start.
-        file.write(
-            MUSIC_FUNNEL_SYNC_STACK
-            if sync_funnel else
-            MUSIC_FUNNEL_RAND_STACK
-        )
-        file.write(MUSIC_FUNNEL_UPDATE_STACK)
+        if sync_funnel:
+            sounds_synch.append(sound_funnel)
+            # The base then needs to resync with itself.
+            sounds_synch.append(sound_base)
+        else:
+            # Randomly choose a start.
+            with sound_funnel.stack_start.build() as builder:
+                with builder.random_offset:
+                    builder.operator("math_random")
+                    builder.input_min("-300")
+                    builder.input_max("0")
+                with builder.delay_output:
+                    builder.operator("sys_output")
+                    builder.input_float("@random_offset.output")
+                    builder.output("delay")
+
+        with sound_funnel.stack_update.build() as builder:
+            builder.import_stack("update_music_stereo")
+            with builder.mixer:
+                builder.mixgroup("unduckedMusic")
+            with builder.volume_fade_in:
+                builder.input_max("3.0")
+                builder.input_map_min("0.05")
+            with builder.volume_fade_out:
+                builder.input_max("0.75")
+                builder.input_map_min("0.05")
+            with builder.volume_lfo_time_scale:
+                builder.input2("0.3")
+            with builder.volume_lfo_scale:
+                builder.input2("0.4")
 
     if has_bounce:
-        file.write('\n')
-        file.write(MUSIC_START.format(name='_gel_bounce', vol='0.5'))
-        write_sound(file, bounce, pack_list, snd_prefix='*')
-        # Fade in fast (we never get false positives, but fade out slow
-        # since this disables when falling back..
-        file.write(MUSIC_GEL_STACK.format(fadein=0.25, fadeout=1.5))
+        sound_bounce = snd.Sound(
+            "music.BEE2_gel_bounce",
+            ['*' + wav for wav in bounce.as_array()],
+            channel=snd.Channel.BACKGROUND,
+            level=snd.Level.SNDLVL_NONE,
+        )
+        sounds.append(sound_bounce)
+        sounds_synch.append(sound_bounce)
 
-    if speed.value:
-        file.write('\n')
-        file.write(MUSIC_START.format(name='_gel_speed', vol='0.5'))
-        write_sound(file, speed, pack_list, snd_prefix='*')
+        # Fade in fast (we never get false positives), but fade out slow
+        # since this disables when falling back..
+        with sound_bounce.stack_update.build() as builder:
+            builder.import_stack("update_music_stereo")
+            with builder.volume_fade_in:
+                builder.input_max("0.25")
+            with builder.volume_fade_out:
+                builder.input_max("1.5")
+
+    if has_speed:
+        sound_speed = snd.Sound(
+            "music.BEE2_gel_speed",
+            ['*' + wav for wav in speed.as_array()],
+            channel=snd.Channel.BACKGROUND,
+            level=snd.Level.SNDLVL_NONE,
+        )
+        sounds.append(sound_speed)
+        sounds_synch.append(sound_speed)
+
         # We need to shut off the sound fast, so portals don't confuse it.
         # Fade in slow so it doesn't make much sound (and also as we get
         # up to speed). We stop almost immediately on gel too.
-        file.write(MUSIC_GEL_STACK.format(fadein=0.5, fadeout=0.1))
+        with sound_speed.stack_update.build() as builder:
+            builder.import_stack("update_music_stereo")
+            with builder.volume_fade_in:
+                builder.input_max("0.5")
+            with builder.volume_fade_out:
+                builder.input_max("0.1")
 
-    return file.getvalue().encode()
+    # If we want syncronisation, do that using opvars to keep it even when
+    # the base track is paused by the funnel.
+    # We sync to the base track if the funnel isn't synced, otherwise the main.
 
+    if sounds_synch:
+        if sync_funnel:
+            sync_point = sound_base
+        else:
+            sync_point = sound_start
 
-def write_sound(
-    file: StringIO,
-    snds: Property,
-    pack_list: PackList,
-    snd_prefix: str='*',
-) -> None:
-    """Write either a single sound, or multiple rndsound.
+        with sync_point.stack_start.build() as builder:
+            with builder.sys_time:
+                builder.operator("get_sys_time")
+            with builder.sync_time:
+                builder.operator("set_opvar_float")
+                builder.opvar(MUSIC_SYNC_OPVAR)
+                builder.input("@sys_time.output_client_time")
 
-    snd_prefix is the prefix for each filename - *, #, @, etc.
-    """
-    if snds.has_children():
-        file.write('"rndwave"\n\t{\n')
-        for snd in snds:
-            file.write(
-                '\t"wave" "{sndchar}{file}"\n'.format(
-                    file=snd.value.lstrip(SOUND_CHARS),
-                    sndchar=snd_prefix,
-                )
-            )
-            pack_list.pack_file('sound/' + snd.value.casefold())
-        file.write('\t}\n')
-    else:
-        file.write(
-            '"wave" "{sndchar}{file}"\n'.format(
-                file=snds.value.lstrip(SOUND_CHARS),
-                sndchar=snd_prefix,
-            )
-        )
-        pack_list.pack_file('sound/' + snds.value.casefold())
+    # The more complicated syncing logic.
+    for script in sounds_synch:
+        with script.stack_start.build() as builder:
+            # Get the time, subtract from now.
+            with builder.sys_time:
+                builder.operator("get_sys_time")
+            with builder.sync_time:
+                builder.operator("get_opvar_float")
+                builder.opvar(MUSIC_SYNC_OPVAR)
+            with builder.sync_delta:
+                builder.operator("math_float")
+                builder.apply("sub")
+                builder.input1("@sys_time.output_client_time")
+                builder.input2("@sync_time.output")
+            # Invert.
+            with builder.sync_negative_delay:
+                builder.operator("math_float")
+                builder.apply("mult")
+                builder.input1("@sync_delta.output")
+                builder.input2("-1.0")
+            # Apply the delay.
+            with builder.delay_output:
+                builder.operator("sys_output")
+                builder.input_float("@sync_negative_delay.output")
+                builder.output("delay")
+
+    file = StringIO()
+
+    for script in sounds:
+        for wav in script.sounds:
+            pack_list.pack_file('sound/' + wav.lstrip(snd.SND_CHARS))
+
+        script.export(file)
+        file.write('\n')
+
+    return file.getvalue().rstrip().encode('utf8')
 
 
 def inject_files() -> Iterator[Tuple[str, str]]:
