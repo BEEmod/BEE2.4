8a634aa74d1bbb988aa68b9b4b3abeb708cc75fd
TeamSpen210 <spencerb21@live.com>
2021-01-30 19:03:44 +1000

    Handle empty slot in signage a little more nicely

--- src/app/signage_ui.py
+++ src/app/signage_ui.py
@@ -199,14 +199,13 @@ def init_widgets(master: ttk.Frame) -> Optional[tk.Misc]:
     def on_hover(slot: dragdrop.Slot[Signage]) -> None:
         """Show the signage when hovered."""
         nonlocal hover_arrow, hover_sign
-        name_label['text'] = (
-            _('Signage: {}').format(slot.contents.name)
-            if slot.contents is not None else
-            ''
-        )
-        hover_sign = slot.contents
-        hover_arrow = True
-        hover_toggle()
+        if slot.contents is not None:
+            name_label['text'] = _('Signage: {}').format(slot.contents.name)
+            hover_sign = slot.contents
+            hover_arrow = True
+            hover_toggle()
+        else:
+            on_leave(slot)
 
     def on_leave(slot: dragdrop.Slot[Signage]) -> None:
         """Reset the visible sign when left."""
