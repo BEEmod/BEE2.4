fdf3d66b2741ab897768af8bbdb7e31a154f43c0
TeamSpen210 <spencerb21@live.com>
2021-02-20 17:34:27 +1000

    Add marisa-trie licenses to credits

--- src/app/helpMenu.py
+++ src/app/helpMenu.py
@@ -85,6 +85,7 @@ Used software / libraries in the BEE2.4:
 * [Pillow {pil_ver}][pillow] by Alex Clark and Contributors
 * [noise (2008-12-15)][perlin_noise] by Casey Duncan
 * [mistletoe {mstle_ver}][mistletoe] by Mi Yu and Contributors
+* [marisa-trie][marisa] by Susumu Yata and Contributors
 * [TKinter {tk_ver}/TTK {ttk_ver}/Tcl {tcl_ver}][tcl]
 * [Python {py_ver}][python]
 
@@ -93,6 +94,7 @@ Used software / libraries in the BEE2.4:
 [pillow]: http://pillow.readthedocs.io
 [perlin_noise]: https://github.com/caseman/noise
 [mistletoe]: https://github.com/miyuchina/mistletoe
+[marisa]: https://github.com/s-yata/marisa-trie
 [tcl]: https://tcl.tk/
 [python]: https://www.python.org/
 
@@ -176,6 +178,47 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
+
+-------
+
+# Marisa-trie
+
+Copyright (c) marisa-trie authors and contributors, 2012-2016
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is furnished
+to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR
+A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+------
+
+# Libmarisa
+
+libmarisa and its command line tools are dual-licensed under the BSD 2-clause license and the LGPL.
+
+#### The BSD 2-clause license
+
+Copyright (c) 2010-2016, Susumu Yata
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+- Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+- Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 '''.format(
     # Inject the running Python version.
     py_ver=platform.python_version(),
