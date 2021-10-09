ab63d6cf656df7f47526b7e6dfc62a7583219150
Spencer Brown <spencerb21@live.com>
2021-10-09 14:58:29 +1000

    Make AND propagate its first flags's unsatisfiability

--- src/precomp/conditions/logical.py
+++ src/precomp/conditions/logical.py
@@ -8,16 +8,16 @@ COND_MOD_NAME = 'Logic'
 
 
 @make_flag('AND')
-def flag_and(vmf: VMF, inst: Entity, flag: Property):
+def flag_and(inst: Entity, flag: Property):
     """The AND group evaluates True if all sub-flags are True."""
-    for sub_flag in flag:
-        if not check_flag(sub_flag, inst):
+    for i, sub_flag in enumerate(flag):
+        if not check_flag(sub_flag, inst, can_skip=i == 0):
             return False
     return True
 
 
 @make_flag('OR')
-def flag_or(vmf: VMF, inst: Entity, flag: Property):
+def flag_or(inst: Entity, flag: Property):
     """The OR group evaluates True if any sub-flags are True."""
     for sub_flag in flag:
         if check_flag(sub_flag, inst):
@@ -26,26 +26,34 @@ def flag_or(vmf: VMF, inst: Entity, flag: Property):
 
 
 @make_flag('NOT')
-def flag_not(vmf: VMF, inst: Entity, flag: Property):
+def flag_not(inst: Entity, flag: Property):
     """The NOT group inverts the value of it's one sub-flag."""
-    if len(flag.value) == 1:
-        return not check_flag(flag[0], inst)
-    return False
+    try:
+        [subflag] = flag
+    except ValueError:
+        return False
+    return not check_flag(subflag, inst)
 
 
 @make_flag('XOR')
-def flag_xor(vmf: VMF, inst: Entity, flag:Property):
+def flag_xor(inst: Entity, flag:Property):
     """The XOR group returns True if the number of true sub-flags is odd."""
     return sum([check_flag(sub_flag, inst) for sub_flag in flag]) % 2 == 1
 
 
 @make_flag('NOR')
-def flag_nor(vmf: VMF, inst: Entity, flag: Property):
+def flag_nor(inst: Entity, flag: Property) -> bool:
     """The NOR group evaluates True if any sub-flags are False."""
-    return not flag_or(vmf, inst, flag)
+    for sub_flag in flag:
+        if check_flag(sub_flag, inst):
+            return True
+    return False
 
 
 @make_flag('NAND')
-def flag_nand(vmf: VMF, inst: Entity, flag: Property):
+def flag_nand(inst: Entity, flag: Property) -> bool:
     """The NAND group evaluates True if all sub-flags are False."""
-    return not flag_and(vmf, inst, flag)
+    for sub_flag in flag:
+        if not check_flag(sub_flag, inst):
+            return True
+    return False
