cb16c5f8986ffb620f59f26b3a0c0a5149dcbee1
Spencer Brown <spencerb21@live.com>
2021-11-25 19:21:56 +1000

    Make integers and floats acceptable for seeds

--- src/precomp/rand.py
+++ src/precomp/rand.py
@@ -12,6 +12,7 @@ from srctools import VMF, Vec, Angle, Entity, logger, Matrix
 # This should be copied to use for specific purposes, never modified.
 MAP_HASH = hashlib.sha256()
 ONE_FLOAT = Struct('f')
+ONE_INT = Struct('<i')
 THREE_FLOATS = Struct('<3f')
 NINE_FLOATS = Struct('<9e')  # Half-precision float, don't need the accuracy.
 THREE_INTS = Struct('<3i')
@@ -74,7 +75,10 @@ def init_seed(vmf: VMF) -> str:
     return b'|'.join(light_names).decode()  # TODO Remove
 
 
-def seed(name: bytes, *values: str | Entity | Vec | Angle | float | bytes | bytearray) -> Random:
+def seed(
+    name: bytes,
+    *values: str | Entity | Vec | Angle | float | bytes | bytearray | bool,
+) -> Random:
     """Initialise a random number generator with these starting arguments.
 
     The name is used to make this unique among other calls, then the arguments
@@ -90,6 +94,9 @@ def seed(name: bytes, *values: str | Entity | Vec | Angle | float | bytes | byte
             algo.update(THREE_FLOATS.pack(round(a, 6), round(b, 6), round(c, 6)))
         elif isinstance(val, float):
             algo.update(ONE_FLOAT.pack(val))
+        elif isinstance(val, int):
+            # If multi-digit, cycle back in.
+            algo.update(ONE_INT.pack(hash(val)))
         elif isinstance(val, Matrix):
             algo.update(NINE_FLOATS.pack(
                 val[0, 0], val[0, 1], val[0, 2],
@@ -102,9 +109,11 @@ def seed(name: bytes, *values: str | Entity | Vec | Angle | float | bytes | byte
             algo.update(THREE_FLOATS.pack(x, y, z))
             p, y, r = Vec.from_str(val['origin'])
             algo.update(THREE_FLOATS.pack(round(p, 6), round(y, 6), round(r, 6)))
+        elif isinstance(val, bool):
+            algo.update(b'\xff' if val else b'\x00')
         else:
             try:
                 algo.update(val)
             except TypeError:
-                raise TypeError(values)
+                raise TypeError(val, values)
     return Random(int.from_bytes(algo.digest(), 'little'))
