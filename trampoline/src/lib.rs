23e8d543aa7373ac7b068f028a5126bff3058c86
Spencer Brown <spencerb21@live.com>
2021-12-10 14:48:37 +1000

    Make trampoline spawn the Python app.

--- trampoline/src/lib.rs
+++ trampoline/src/lib.rs
@@ -1,6 +1,7 @@
 // The actual common logic.
 use std::env;
 use std::fs;
+use std::process;
 
 #[derive(Debug)]
 enum Config {
@@ -22,9 +23,30 @@ fn parse_config(conf: String) -> Config {
 
 pub fn run_compiler(comp_name: &str) {
     println!("BEE compiler hook for {} started.", comp_name);
-    let p2_args: Vec<String> = env::args().collect();
 
     // Grab the config left by the app.
     let conf = parse_config(fs::read_to_string("bee2/app_loc.cfg").expect("No BEE config file."));
-    println!("Config: {:?}", conf)
+    println!("Config: {:?}", conf);
+    let mut cmd;
+    match conf {
+        Config::Frozen(exe) => {
+            cmd = process::Command::new(exe);
+        }
+        Config::PySource {exe, script} => {
+            cmd = process::Command::new(exe);
+            cmd.arg(script);
+        }
+    };
+    // Add on the compiler to use.
+    cmd.arg(comp_name);
+    // Remove ourselves from the args list.
+    cmd.args(env::args().skip(1));
+    println!("Spawning compiler: {:?}", cmd);
+    let result = cmd
+        .spawn().expect("Could not start compiler.")
+        .wait().expect("Could not wait for compiler.");
+    process::exit(match result.code() {
+        Some(code) => code,
+        None => { eprintln!("Terminated by signal."); 1}
+    });
 }
