45015e0934ae968f775a15f33f6c55d98b1d3469
Spencer Brown <spencerb21@live.com>
2021-12-22 18:54:34 +1000

    Rewrite tree class to allow multiple items with the same bbox.
    TODO: Test this code

--- src/tree.py
+++ src/tree.py
@@ -1,7 +1,8 @@
 """Wraps the Rtree package, adding typing and usage of our Vec class."""
 from srctools.math import Vec
-from typing import Generic, TypeVar, Iterator
+from typing import Generic, TypeVar, Iterator, List, Tuple, Union
 
+import attr
 from rtree import index
 
 ValueT = TypeVar('ValueT')
@@ -9,54 +10,74 @@ PROPS = index.Property()
 PROPS.dimension = 3
 
 
+@attr.frozen
+class ValueHolder(Generic[ValueT]):
+    """Holds the list of values."""
+    values: List[ValueT]
+    min_x: float
+    min_y: float
+    min_z: float
+    max_x: float
+    max_y: float
+    max_z: float
+
+
 class RTree(Generic[ValueT]):
-    """A 3-dimensional R-Tree."""
+    """A 3-dimensional R-Tree. Multiple values with the same bbox are allowed."""
     def __init__(self) -> None:
         self.tree = index.Index(properties=PROPS)
-        self.data: dict[int, tuple[
-            tuple[float, float, float,
-                  float, float, float],
-            ValueT,
-        ]] = {}
+        # id(holder) -> holder.
+        # We can't store the object directly in the tree.
+        self._by_id: dict[int, ValueHolder[ValueT]] = {}
+        self._by_coord: dict[
+            tuple[float, float, float, float, float, float],
+            ValueHolder[ValueT]
+        ] = {}
 
-    def __iter__(self) -> 'tuple[Vec, Vec, ValueT]':
+    def __iter__(self) -> Tuple[Vec, Vec, ValueT]:
         """Iterating over the tree returns each bbox and value associated with it."""
-        for (min_x, min_y, min_z, max_x, max_y, max_z), val in self.data.values():
-            yield Vec(min_x, min_y, min_z), Vec(max_x, max_y, max_z), val
+        for holder in self._by_id.values():
+            mins = Vec(holder.min_x, holder.min_y, holder.min_z)
+            maxes = Vec(holder.max_x, holder.max_y, holder.max_z)
+            for val in holder.values:
+                yield mins.copy(), maxes.copy(), val
 
     def insert(self, p1: Vec, p2: Vec, value: ValueT) -> None:
         """Add the specified value occupying the specified region."""
-        key = id(value)
-        try:
-            old_coords, _ = self.data.pop(key)
-        except KeyError:
-            pass
-        else:
-            self.tree.delete(key, old_coords)
         mins, maxs = Vec.bbox(p1, p2)
         coords = (mins.x, mins.y, mins.z, maxs.x, maxs.y, maxs.z)
-        self.data[key] = coords, value
-        self.tree.insert(key, coords)
-
-    def delete(self, value: ValueT) -> None:
-        """Remove the specified value from the tree."""
         try:
-            coords, _ = self.data.pop(id(value))
+            holder = self._by_coord[coords]
         except KeyError:
-            raise KeyError(value) from None
-        self.tree.delete(id(value), coords)
+            # Make one.
+            holder = ValueHolder([value], *coords)
+            self._by_id[id(holder)] = self._by_coord[coords] = holder
+            self.tree.insert(id(holder), coords)
+        else:
+            # Append if not already present.
+            if value not in holder.values:
+                holder.values.append(value)
 
-    def _get_val(self, ind: int) -> ValueT:
-        return self.data[ind][1]
+    def remove(self, p1: Vec, p2: Vec, value: ValueT) -> None:
+        """Remove the specified value from the tree."""
+        mins, maxs = Vec.bbox(p1, p2)
+        coords = (mins.x, mins.y, mins.z, maxs.x, maxs.y, maxs.z)
+        try:
+            holder = self._by_coord[coords]
+            holder.values.remove(value)
+        except LookupError:
+            raise KeyError(mins, maxs, value) from None
+        # Removed, check to see if the holder is empty, and we can discard.
+        if not holder.values:
+            del self._by_id[id(holder)]
+            del self._by_coord[coords]
+            self.tree.delete(id(holder), coords)
 
     def find_bbox(self, p1: Vec, p2: Vec) -> Iterator[ValueT]:
         """Find all values intersecting the given bounding box."""
         mins, maxs = Vec.bbox(p1, p2)
-        dat = self.data
-        return (
-            dat[val_id][1] for val_id in
-            self.tree.intersection((*mins, *maxs))
-        )
+        for holder_id in self.tree.intersection((*mins, *maxs)):
+            yield from self._by_id[holder_id].values
 
     def find_nearest(self, point: Vec, min_count: int = 1) -> ValueT:
         """Find the values nearest to a point.
@@ -64,8 +85,5 @@ class RTree(Generic[ValueT]):
         Return at least the specified number of points - if equidistant more
         may be returned to break the tie.
         """
-        dat = self.data
-        return (
-            dat[val_id][1] for val_id in
-            self.tree.nearest((point.x, point.y, point.z), min_count)
-        )
+        for holder_id in self.tree.nearest((point.x, point.y, point.z), min_count):
+            yield from self._by_id[holder_id].values
