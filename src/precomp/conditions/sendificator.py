d0a281c048304c0e371c1cf824fb7c6159a8bd2d
Spencer Brown <spencerb21@live.com>
2022-07-18 17:04:13 +1000

    Fix Sendificators when multiple are connected to the same laser
    If only Sendificators are connected to a laser, we force it enabled,
    but if there were multiple all but the last to execute wouldn't be
    enabled.

--- src/precomp/conditions/sendificator.py
+++ src/precomp/conditions/sendificator.py
@@ -1,5 +1,7 @@
 """Implement special support """
 from __future__ import annotations
+from collections import defaultdict
+
 from precomp import connections, conditions
 import srctools.logger
 from srctools import Property, Entity, VMF, Vec, Output, Angle, Matrix
@@ -10,6 +12,8 @@ LOGGER = srctools.logger.get_logger(__name__, alias='cond.sendtor')
 
 # Laser name -> offset, normal
 SENDTOR_TARGETS: dict[str, tuple[Vec, Vec]] = {}
+# Laser name -> relays created.
+SENDTOR_RELAYS: dict[str, list[Entity]] = defaultdict(list)
 
 
 @conditions.make_result('SendificatorLaser')
@@ -26,8 +30,8 @@ def res_sendificator_laser(res: Property):
 @conditions.make_result('Sendificator')
 def res_sendificator(vmf: VMF, inst: Entity):
     """Implement Sendificators."""
-    # For our version, we know which sendtor connects to what laser,
-    # so we can couple the logic together (avoiding @sendtor_mutex).
+    # For our version, we know which Sendificator connects to what laser,
+    # so we can couple the logic together (avoiding `@sendtor_mutex`).
 
     sendtor_name = inst['targetname']
     sendtor = connections.ITEMS[sendtor_name]
@@ -70,9 +74,13 @@ def res_sendificator(vmf: VMF, inst: Entity):
             # it off when inputs are connected, which is annoying.
             las_item.inst.fixup['$start_enabled'] = '1'
             is_on = True
+            # If any other relays were made before, set them off too.
+            for relay in SENDTOR_RELAYS[las_item.name]:
+                relay['StartDisabled'] = False
         else:
             is_on = las_item.inst.fixup.bool('$start_enabled')
 
         relay['StartDisabled'] = not is_on
+        SENDTOR_RELAYS[las_item.name].append(relay)
         las_item.enable_cmd += (Output('', relay_name, 'Enable'),)
         las_item.disable_cmd += (Output('', relay_name, 'Disable'),)
