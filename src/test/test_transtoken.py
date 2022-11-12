f48b412530c2cd5e542b85bf1381a3baafff3584
Spencer Brown <spencerb21@live.com>
2022-11-12 17:54:20 +1000

    Fix test

--- src/test/test_transtoken.py
+++ src/test/test_transtoken.py
@@ -1,6 +1,6 @@
 """Test the localisation system."""
 from srctools import EmptyMapping
-from app.localisation import TransToken, NS_GAME, NS_UI, NS_UNTRANSLATED
+from transtoken import TransToken, NS_GAME, NS_UI, NS_UNTRANSLATED
 
 
 def token_constructor() -> None:
