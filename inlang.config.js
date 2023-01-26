09fbaa136f9b5def2fbc67e802416c20faf540cb
jannesblobel <72493222+jannesblobel@users.noreply.github.com>
2023-01-26 23:30:15 +0100

    init inlang config

---
+++ inlang.config.js
@@ -0,0 +1,24 @@
+// filename: inlang.config.js
+
+export async function defineConfig(env) {
+  // importing a plugin
+  const plugin = await env.$import(
+    "https://cdn.jsdelivr.net/gh/jannesblobel/inlang-plugin-po@1/dist/index.js"
+  );
+
+  // most plugins require additional config, read the plugins documentation
+  // for the required config and correct usage.
+  const pluginConfig = {
+    pathPattern: "./i18n/{language}/.po",
+    referenceResourcePath: "./i18n/BEE2/.po",
+  };
+
+  return {
+    referenceLanguage: "BEE2",
+    languages: ["es", "fr", "ja", "pl", "ru", "zh_cn"],
+    readResources: (args) =>
+      plugin.readResources({ ...args, ...env, pluginConfig }),
+    writeResources: (args) =>
+      plugin.writeResources({ ...args, ...env, pluginConfig }),
+  };
+}
