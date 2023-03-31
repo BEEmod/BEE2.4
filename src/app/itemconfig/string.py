36cd08016cf20b4e2aa1716219f31df6629b93e9
Spencer Brown <spencerb21@live.com>
2023-03-31 16:46:22 +1000

    Register text widget under new system

--- src/app/itemconfig/string.py
+++ src/app/itemconfig/string.py
@@ -4,13 +4,17 @@ import tkinter as tk
 from srctools import Keyvalues
 from tkinter import ttk
 
-from app.itemconfig import UpdateFunc, WidgetLookup, nop_update
+from app import itemconfig
 
 
-@WidgetLookup('string', 'str')
-async def widget_string(parent: tk.Widget, var: tk.StringVar, conf: Keyvalues) -> tuple[tk.Widget, UpdateFunc]:
+TYPE = itemconfig.register_no_conf('string', 'str')
+
+
+@itemconfig.ui_single_no_conf(TYPE)
+@itemconfig.WidgetLookup('string', 'str')
+async def widget_string(parent: tk.Widget, var: tk.StringVar, conf: Keyvalues) -> tuple[tk.Widget, itemconfig.UpdateFunc]:
     """Simple textbox for entering text."""
     return ttk.Entry(
         parent,
         textvariable=var,
-    ), nop_update
+    ), itemconfig.nop_update
