f4fc894f6d991e65a4158a915d919b5e9736bdd7
Spencer Brown <spencerb21@live.com>
2023-04-02 16:06:12 +1000

    Fix export here

--- src/packages/widgets.py
+++ src/packages/widgets.py
@@ -345,7 +345,7 @@ class ConfigGroup(packages.PakObject, allow_mult=True, needs_foreground=True):
                 if s_wid.has_values:
                     config_section[s_wid.id] = s_wid.value
             for m_wid in conf.multi_widgets:
-                for num, value in m_wid.values:
+                for num, value in m_wid.values.items():
                     config_section[f'{m_wid.id}_{num}'] = value
             if not config_section:
                 del CONFIG[conf.id]
