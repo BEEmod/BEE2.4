10445863f433cae743a63f62fa6de44807c22264
Spencer Brown <spencerb21@live.com>
2023-08-16 13:30:35 +1000

    Add ErrorUI class, partially test

--- src/app/errors.py
+++ src/app/errors.py
@@ -1,11 +1,25 @@
 """Handles displaying errors to the user that occur during operations."""
 from __future__ import annotations
 
-from typing import final
+import types
+from collections.abc import Iterable
+from contextlib import contextmanager
+from typing import Awaitable, Callable, ClassVar, Generator, Protocol, final, overload
+from exceptiongroup import ExceptionGroup, BaseExceptionGroup
 
 import attrs
+import trio
+import srctools.logger
 
 from transtoken import TransToken
+
+
+LOGGER = srctools.logger.get_logger(__name__)
+DEFAULT_TITLE = TransToken.ui("BEEmod Error")
+DEFAULT_DESC = TransToken.ui_plural(
+    "An error occurred while performing this task:",
+    "Multiple errors occurred while performing this task:",
+)
 
 
 @final
@@ -20,3 +34,112 @@ class AppError(Exception):
 
     def __str__(self) -> str:
         return f"AppError: {self.message}"
+
+
+class Handler(Protocol):
+    """The signature of handler functions."""
+    def __call__(self, title: TransToken, desc: TransToken, errors: list[AppError]) -> Awaitable[object]:
+        ...
+
+@final
+class ErrorUI:
+    """A context manager which handles processing the errors."""
+    title: TransToken
+    desc: TransToken
+    _errors: list[AppError]
+
+    _handler: ClassVar[Handler | None] = None
+
+    @classmethod
+    @contextmanager
+    def install_handler(
+        cls, handler: Handler,
+    ) -> Generator[None, None, None]:
+        """Install the handler for displaying errors."""
+        if cls._handler is not None:
+            raise ValueError("Handler already installed!")
+        try:
+            cls._handler = handler
+            yield
+        finally:
+            cls._handler = None
+
+    def __init__(
+        self,
+        title: TransToken = DEFAULT_TITLE,
+        desc: TransToken = DEFAULT_DESC,
+    ) -> None:
+        """Create a UI handler. install_handler() must already be running."""
+        if self._handler is None:
+            LOGGER.warning("ErrorUI initialised with no handler running!")
+        self.title = title
+        self.desc = desc
+        self._errors = []
+
+    def __repr__(self) -> str:
+        return f"<ErrorUI, title={self.title}, {len(self._errors)} errors>"
+
+    @property
+    def failed(self) -> bool:
+        """Check if the operation has failed."""
+        return bool(self._errors)
+
+    def add(self, error: AppError | ExceptionGroup | BaseExceptionGroup) -> None:
+        """Log an error having occurred, while still running code.
+
+        If an exception group is passed, this will extract the AppErrors, reraising others.
+        """
+        if isinstance(error, AppError):
+            self._errors.append(error)
+        else:
+            matching, rest = error.split(AppError)
+            if matching is not None:
+                self._errors.extend(matching.exceptions)
+            if rest is not None:
+                raise rest
+
+    async def __aenter__(self) -> ErrorUI:
+        return self
+
+    async def __aexit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_val: BaseException | None,
+        exc_tb: types.TracebackType | None,
+    ) -> bool:
+        if isinstance(exc_val, AppError):
+            self._errors.append(exc_val)
+        elif isinstance(exc_val, BaseExceptionGroup):
+            matching, rest = exc_val.split(AppError)
+            if rest is not None:
+                # Another actual exception occurred. Let the exception group
+                # propagate unchanged.
+                return False
+            if matching is not None:
+                self._errors.extend(matching.exceptions)
+        elif exc_val is not None:
+            # Caught something else, don't suppress.
+            if self._errors:
+                # Combine both in an exception group.
+                raise BaseExceptionGroup(
+                    "ErrorUI block raised",
+                    [exc_val, *self._errors],
+                )
+
+            # Just some other exception, leave it unaltered.
+            return False
+
+        if self._errors:
+            desc = self.desc.format(n=len(self._errors))
+            # We had an error.
+            if self._handler is None:
+                LOGGER.error(
+                    "ErrorUI block failed, but no handler installed!\ntitle={}\ndesc={}\n{}",
+                    self.title,
+                    desc,
+                    "\n".join(map(str, self._errors)),
+                )
+            else:
+                # Do NOT pass self!
+                await ErrorUI._handler(self.title, desc, self._errors)
+        return True
--- src/test/app/test_errors.py
+++ src/test/app/test_errors.py
@@ -1,5 +1,17 @@
-from app.errors import AppError
+from exceptiongroup import ExceptionGroup
+
+import pytest
+import trio
+
+from app.errors import AppError, ErrorUI
 from transtoken import TransToken
+
+
+@pytest.fixture(scope="module", autouse=True)
+def reset_handler() -> None:
+    """To ensure only relevant tests fail, reset the handler manually."""
+    print("Resetting handler.")
+    ErrorUI._handler = None
 
 
 def test_exception() -> None:
@@ -11,3 +23,89 @@ def test_exception() -> None:
     assert err.args == (msg, )
 
     assert str(err) == "AppError: The message"
+
+async def handler_fail(title: TransToken, desc: TransToken, errors: list[AppError]) -> None:
+    """Should never be used."""
+    pytest.fail("Handler called!")
+
+
+def test_handler_install() -> None:
+    """Test installing correctly prevents reentrancy and handles exceptions."""
+    assert ErrorUI._handler is None
+    with ErrorUI.install_handler(handler_fail):
+        assert ErrorUI._handler is handler_fail
+
+    assert ErrorUI._handler is None
+
+    with pytest.raises(ZeroDivisionError):
+        assert ErrorUI._handler is None
+        with ErrorUI.install_handler(handler_fail):
+            assert ErrorUI._handler is handler_fail
+            raise ZeroDivisionError
+
+    assert ErrorUI._handler is None
+
+    with ErrorUI.install_handler(handler_fail):
+        with pytest.raises(ValueError, match="already installed"):
+            with ErrorUI.install_handler(handler_fail):
+                pass
+        assert ErrorUI._handler is handler_fail
+    assert ErrorUI._handler is None
+
+
+async def test_success() -> None:
+    """Test code running successfully."""
+    with ErrorUI.install_handler(handler_fail):
+        async with ErrorUI() as error_block:
+            pass
+        assert not error_block.failed
+    assert ErrorUI._handler is None
+
+
+async def test_nonfatal() -> None:
+    """Test the behaviour of non-fatal .add() errors."""
+    orig_title = TransToken.untranslated("The title")
+    orig_desc = TransToken.untranslated("the description, n={n}")
+    caught_errors: list[AppError] = []
+
+    async def catch(title: TransToken, desc: TransToken, errors: list[AppError]) -> None:
+        """Catch the errors that occur."""
+        assert title is orig_title
+        assert str(desc) == "the description, n=4"
+        caught_errors.extend(errors)
+
+    exc1 = AppError(TransToken.untranslated("Error 1"))
+    exc2 = AppError(TransToken.untranslated("Error 2"))
+    exc3 = AppError(TransToken.untranslated("Error 3"))
+    exc4 = AppError(TransToken.untranslated("Error 4"))
+    unrelated = BufferError("Whatever")
+
+    task: list[str] = []
+    success = False
+    with ErrorUI.install_handler(catch):
+        async with ErrorUI(orig_title, orig_desc) as error_block:
+            assert not error_block.failed
+            task.append("before")
+
+            error_block.add(exc1)
+            assert error_block.failed  # now failed.
+            task.append("mid")
+
+            error_block.add(ExceptionGroup("two", [exc2, exc3]))
+            # Does not raise.
+
+            with pytest.raises(ExceptionGroup) as reraised:
+                error_block.add(ExceptionGroup("stuff", [exc4, unrelated]))
+            # unrelated should have been re-raised.
+            assert isinstance(reraised.value, ExceptionGroup)
+            assert reraised.value.message == "stuff"
+            assert reraised.value.exceptions == (unrelated, )
+
+            task.append("after")
+            assert error_block.failed  # still failed.
+        success = True  # The async-with did not raise.
+
+    assert success
+    assert task == ["before", "mid", "after"]
+    assert error_block.failed
+    assert caught_errors == [exc1, exc2, exc3, exc4]
