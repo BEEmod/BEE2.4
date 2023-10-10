7b2995135629a353a7cacb91345e00636bcbee51
Spencer Brown <spencerb21@live.com>
2023-10-10 11:28:04 +1000

    Move corridor widget creation to ui_tk

--- src/app/corridor_selector.py
+++ src/app/corridor_selector.py
@@ -77,13 +77,6 @@ IconT = TypeVar('IconT', bound=Icon)
 
 class Selector(Generic[IconT]):
     """Corridor selection UI."""
-    win: tk.Toplevel
-
-    # Widgets to display info about the corridor on the right side.
-    wid_image: ttk.Label
-    wid_title: ttk.Label
-    wid_authors: ttk.Label
-    wid_desc: tkRichText
     # When you click a corridor, it's saved here and displayed when others aren't
     # moused over. Reset on style/group swap.
     sticky_corr: Optional[corridor.CorridorUI]
@@ -102,137 +95,12 @@ class Selector(Generic[IconT]):
     conf_id: str
 
     def __init__(self, packset: packages.PackagesSet, tk_img: TKImages) -> None:
+        super().__init__(packset)
         self.sticky_corr = None
         self.img_ind = 0
         self.cur_images = None
-        self.slots = []
-        self.sel_count = 0
-        self.sel_handle_moving = False
-
-        self.win = tk.Toplevel(TK_ROOT, name='corridor')
-        self.win.withdraw()
-        self.win.wm_protocol("WM_DELETE_WINDOW", self.hide)
-        localisation.set_win_title(self.win, TransToken.ui('BEEmod - Select Corridor'))
-
-        self.win.rowconfigure(0, weight=1)
-        self.win.columnconfigure(0, weight=1)
-
-        frm_left = ttk.Frame(self.win, name='frm_left')
-        frm_left.grid(row=0, column=0, sticky='nsew')
-
-        frm_right = ttk.Frame(self.win, name='frm_right')
-        frm_right.columnconfigure(0, weight=1)
-        frm_right.grid(row=0, column=1, sticky='ns')
-        frm_img = ttk.Frame(frm_right, relief='raised', width=2)
-        frm_img.grid(row=0, column=0, sticky='ew')
-
-        sel_img = self._sel_img
-        self.wid_image_left = ttk.Button(frm_img, name='imgLeft', command=lambda: sel_img(-1))
-        self.wid_image = ttk.Label(frm_img, name='img')
-        self.wid_image_right = ttk.Button(frm_img, name='imgRight', command=lambda: sel_img(1))
-
-        self.wid_image_left.grid(row=0, column=0, sticky='ns')
-        self.wid_image.grid(row=0, column=1, sticky='nsew')
-        self.wid_image_right.grid(row=0, column=2, sticky='ns')
-
-        tk_tools.bind_mousewheel(self.wid_image, sel_img)
-
-        tk_img.apply(self.wid_image_left, IMG_ARROW_LEFT)
-        tk_img.apply(self.wid_image, IMG_CORR_BLANK)
-        tk_img.apply(self.wid_image_right, IMG_ARROW_RIGHT)
-        self.tk_img = tk_img
-
-        self.wid_title = ttk.Label(
-            frm_right,
-            name='title',
-            justify='center',
-            anchor='center',
-            font=("Helvetica", 12, "bold"),
-            text='',
-        )
-        self.wid_title.grid(row=1, column=0, columnspan=2, sticky='ew')
-        self.wid_authors = ttk.Label(
-            frm_right,
-            name='authors',
-            justify='center',
-            anchor='center',
-            text='',
-        )
-        self.wid_authors.grid(row=2, column=0, columnspan=2, sticky='ew')
-
-        self.wid_desc = tkRichText(frm_right)
-        desc_scroll = tk_tools.HidingScroll(frm_right, orient='vertical', command=self.wid_desc.yview)
-        self.wid_desc['yscrollcommand'] = desc_scroll.set
-        self.wid_desc.grid(row=3, column=0, sticky='nsew')
-        desc_scroll.grid(row=3, column=1, sticky='ns')
-        frm_right.rowconfigure(3, weight=1)
-
-        localisation.set_text(
-            ttk.Button(frm_right, name='closer', command=self.hide),
-            TransToken.ui('Close'),
-        ).grid(row=4, column=0, columnspan=2)
-
-        conf = config.APP.get_cur_conf(UIState, default=UIState())
-        if conf.width > 0 and conf.height > 0:
-            self.win.geometry(f'{conf.width}x{conf.height}')
-
-        button_frm = ttk.Frame(frm_left)
-        button_frm.grid(row=0, column=0, columnspan=3)
-        self.btn_mode = tk_tools.EnumButton(
-            button_frm, conf.last_mode,
-            (GameMode.SP, TransToken.ui('SP')),
-            (GameMode.COOP, TransToken.ui('Coop')),
-        )
-        self.btn_direction = tk_tools.EnumButton(
-            button_frm, conf.last_direction,
-            (Direction.ENTRY, TransToken.ui('Entry')),
-            (Direction.EXIT, TransToken.ui('Exit')),
-        )
-        self.btn_orient = tk_tools.EnumButton(
-            button_frm, conf.last_orient,
-            (Orient.FLAT, TransToken.ui('Flat')),
-            (Orient.UP, TransToken.ui('Upward')),
-            (Orient.DN, TransToken.ui('Downward')),
-        )
-        self.btn_mode.frame.grid(row=0, column=0, padx=8)
-        self.btn_direction.frame.grid(row=0, column=1, padx=8)
-        self.btn_orient.frame.grid(row=0, column=2, padx=8)
-
-        refresh = self.refresh
-        self.btn_mode.on_changed.register(refresh)
-        self.btn_direction.on_changed.register(refresh)
-        self.btn_orient.on_changed.register(refresh)
-
-        canv_frame = ttk.Frame(frm_left, name='canv_frame', relief="sunken")
-        canv_frame.grid(row=1, column=0, columnspan=3, sticky='nsew', ipadx=8, ipady=8)
-        canv_frame.rowconfigure(0, weight=1)
-        canv_frame.columnconfigure(0, weight=1)
-        frm_left.columnconfigure(0, weight=1)
-        frm_left.rowconfigure(1, weight=1)
-
-        self.canvas = tk.Canvas(canv_frame)
-        self.canvas.grid(row=0, column=0, sticky='nsew')
-        scrollbar = ttk.Scrollbar(canv_frame, orient='vertical', command=self.canvas.yview)
-        scrollbar.grid(row=0, column=1, sticky='ns')
-        self.canvas['yscrollcommand'] = scrollbar.set
-        self.unsel_div = self.canvas.create_line(
-            0, 0, 1024, 0,
-            width=2,
-            fill='black',
-        )
-
-        # Avoid making self a cell var.
-        self.canvas.bind('<Configure>', tk_tools.make_handler(self.evt_resized))
-
-        self.help_lbl = ttk.Label(self.canvas)
-        localisation.set_text(self.help_lbl, TransToken.ui(
-            "Drag corridors to the 'selected' and 'unused' areas to specify which are used. "
-            "Ingame, a random corridor from the 'selected' group will be picked for each map."
-        ))
-        self.help_lbl_win = self.canvas.create_window(0, 0, anchor='nw', window=self.help_lbl)
-
-        tk_tools.add_mousewheel(self.canvas, self.win)
-        self.load_corridors(packset)
+        self.icons = []
+        self.corr_list = []
 
     def show(self) -> None:
         """Display the window."""
