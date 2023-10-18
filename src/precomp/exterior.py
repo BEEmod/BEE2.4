9fe0cb3e3af29657ee46a2d23770647ef71d12ee
Konclan <classicblazes@gmail.com>
2023-10-18 13:07:38 -0500

    testing

--- src/precomp/exterior.py
+++ src/precomp/exterior.py
@@ -1,10 +1,11 @@
 import srctools.logger
-from srctools import VMF, Vec
+from srctools import VMF, Vec, FrozenVec, Angle, Matrix
 
 import consts
 import precomp.options
 import precomp.pathing
-from precomp.brushLoc import POS
+from precomp.pathing import NodeKind, GenericNode
+from precomp.brushLoc import POS, world_to_grid, grid_to_world, Block
 from precomp.corridor import Info as CorrInfo
 
 
@@ -13,16 +14,24 @@ LOGGER = srctools.logger.get_logger(__name__)
 EX_SIZE_MAX = Vec(69, 69, 69)*128
 EX_SIZE_MIN = Vec(32, 32, 32)*128
 
+CATWALKS = [
+    NodeKind(name= 'STRAIGHT', end_pos= FrozenVec(1, 0, 0)),
+    NodeKind(name='CORNER_LEFT', end_pos= FrozenVec(1, 0, 0), angle= Angle(0, -90, 0)),
+    NodeKind(name='CORNER_RIGHT', end_pos= FrozenVec(1, 0, 0), angle= Angle(0, 90, 0)),
+    NodeKind(name='STAIR_UP', end_pos= FrozenVec(2, 0, 1)),
+    NodeKind(name='STAIR_DN', end_pos= FrozenVec(2, 0, -1)),
+]
+
 def make_exterior(vmf: VMF, coll, info: CorrInfo) -> None:
     """Generate the exterior of the map: pits, catwalks, tubes, etc."""
-    if not precomp.options.get(bool, 'extend_chamber'):
-        return None
+    # if not precomp.options.get(bool, 'extend_chamber'):
+    #     return None
 
     # Make the box that contains the map
-    outer_bounds = make_exterior_shell(vmf)
+    map_bounds = make_exterior_shell(vmf)
 
     # Move Elevators to valid location if possible and then generate elevator shell
-    place_entrance_exit(vmf, info, outer_bounds)
+    place_entrance_exit(info, map_bounds)
 
     # Open Walls and add square beams
 
@@ -41,15 +50,44 @@ def make_exterior_shell(vmf : VMF):
         max(EX_SIZE_MIN / 2, min(EX_SIZE_MAX / 2, (pos_max - bbox_origin) + size_pad))
     )
 
-    outer_bounds = Vec.bbox(bbox_lower + bbox_origin, bbox_upper + bbox_origin)
+    map_bounds = Vec.bbox(bbox_lower + bbox_origin, bbox_upper + bbox_origin)
 
-    for solid in vmf.make_hollow(*outer_bounds, 16, consts.Tools.NODRAW, consts.Tools.BLACK):
+    for solid in vmf.make_hollow(*map_bounds, 16, consts.Tools.NODRAW, consts.Tools.BLACK):
         vmf.add_brush(solid)
 
-    return outer_bounds
+    return map_bounds
 
     # vmf.add_brush(vmf.make_prism(pos_min, pos_max, consts.Tools.SKIP).solid)
 
-def place_entrance_exit(vmf: VMF, info: CorrInfo, bounds):
+def place_entrance_exit(info: CorrInfo, map_bounds):
+
+    entry_pos = Vec.from_str(info.inst_entry['origin'])
+    entry_orient = Matrix.from_angstr(info.inst_entry['angles'])
+
+    start_pos = world_to_grid(Vec(-640, 0, 0) @ entry_orient + entry_pos)
+    start_angle = Angle(0, round(entry_orient.to_angle().yaw + 180) % 360, 0)
+
+    end_pos = FrozenVec(0, 27, start_pos.z)
+    end_angle = Angle(0, 0, 0)
+
+    start_node = GenericNode( start_pos, start_angle, CATWALKS[0])
+    end_node = GenericNode( end_pos, end_angle, CATWALKS[0])
+
+    blockers = [
+        Block.VOID
+    ]
+
     """Place the map entrance and exit"""
-    precomp.pathing.test(vmf, info, bounds)
+    path = precomp.pathing.test(start_node, end_node, CATWALKS, info, map_bounds, blockers)
+    for node in path:
+        # mdl, off, yaw_val = CATWALKS[node.trace]
+        # ang = node.yaw.orient.to_angle()
+        # ang.yaw += yaw_val
+        VMF.create_ent(
+            'info_particle_system',
+            origin=grid_to_world(node.pos.thaw()),
+            angles=node.orient.to_angle(),
+            targetname = str(node.trace.name)
+        )
+
+    return
--- src/precomp/pathing.py
+++ src/precomp/pathing.py
@@ -2,11 +2,11 @@
 import functools
 import math
 from enum import Enum, auto
-from typing import Iterable
+from typing import Iterable, Union, ClassVar, Any
 
 import astar
 import attrs
-from srctools import Angle, FrozenMatrix, FrozenVec, Matrix, VMF, Vec, logger
+from srctools import Angle, FrozenMatrix, FrozenVec, Matrix, VMF, Vec, logger, FrozenAngle
 
 import utils
 from precomp.corridor import Info as CorrInfo
@@ -14,77 +14,85 @@ from .brushLoc import Block, grid_to_world, world_to_grid, POS
 
 LOGGER = logger.get_logger(__name__)
 
-@utils.freeze_enum_props
-class Yaw(Enum):
-    EAST = 0
-    NORTH = 90
-    WEST = 180
-    SOUTH = 270
+# @utils.freeze_enum_props
+# class Yaw(Enum):
+#     EAST = 0
+#     NORTH = 90
+#     WEST = 180
+#     SOUTH = 270
+#
+#     @property
+#     def orient(self) -> FrozenMatrix:
+#         """Rotate around this yaw."""
+#         return FrozenMatrix.from_yaw(self.value)
+#
+#     @property
+#     def left(self) -> 'Yaw':
+#         return Yaw((self.value - 90) % 360)
+#
+#     @property
+#     def right(self) -> 'Yaw':
+#         return Yaw((self.value + 90) % 360)
+
+class NodeKind:
+    def __init__(self, name: str, end_pos: Union[Vec, FrozenVec], instance =  None, angle = None, bbox = None, cost = None):
+        self.name = name
+        self.end_pos = end_pos
+        self.instance = instance if instance is not None else ""
+        self.angle = angle if angle is not None else Angle(0, 0, 0)
+        self.bbox = Vec.iter_grid(bbox.bbox_min, bbox.bbox_max).append(end_pos) if bbox is not None else end_pos
+        self.cost = cost if cost is not None else 0
 
     @property
     def orient(self) -> FrozenMatrix:
         """Rotate around this yaw."""
-        return FrozenMatrix.from_yaw(self.value)
-
-    @property
-    def left(self) -> 'Yaw':
-        return Yaw((self.value - 90) % 360)
-
-    @property
-    def right(self) -> 'Yaw':
-        return Yaw((self.value + 90) % 360)
-
-class Kind(Enum):
-    STRAIGHT = auto()
-    CORNER_LEFT = auto()
-    CORNER_RIGHT = auto()
-    STAIR_UP = auto()
-    STAIR_DN = auto()
-
-    @property
-    def is_stair(self) -> bool:
-        return self.name.startswith('STAIR')
-
-    @property
-    def is_corner(self) -> bool:
-        return self.name.startswith('CORNER')
+        return FrozenMatrix.from_angle(self.angle)
 
 
-PROPS = {
-    Kind.STRAIGHT: ('models/props_bts/hanging_walkway_128a.mdl', FrozenVec(0, 0, -64), 90),
-    Kind.CORNER_LEFT: ('models/props_bts/hanging_walkway_l.mdl', FrozenVec(0, 0, -64), 0),
-    Kind.CORNER_RIGHT: ('models/props_bts/hanging_walkway_l.mdl', FrozenVec(0, 0, -64), 90),
-    Kind.STAIR_UP: ('models/bee2/catwalk/factory_hanging_stairs_128_extended.mdl', FrozenVec(-64, 0, -128), 270),
-    Kind.STAIR_DN: ('models/bee2/catwalk/overgrown/hanging_stair_128.mdl', FrozenVec(-64, 0, 0), 90),
-}
+# PROPS = {
+#     Kind.STRAIGHT: ('models/props_bts/hanging_walkway_128a.mdl', FrozenVec(0, 0, -64), 90),
+#     Kind.CORNER_LEFT: ('models/props_bts/hanging_walkway_l.mdl', FrozenVec(0, 0, -64), 0),
+#     Kind.CORNER_RIGHT: ('models/props_bts/hanging_walkway_l.mdl', FrozenVec(0, 0, -64), 90),
+#     Kind.STAIR_UP: ('models/bee2/catwalk/factory_hanging_stairs_128_extended.mdl', FrozenVec(-64, 0, -128), 270),
+#     Kind.STAIR_DN: ('models/bee2/catwalk/overgrown/hanging_stair_128.mdl', FrozenVec(-64, 0, 0), 90),
+# }
+
+# @attrs.frozen
+# class CatwalkNode:
+#     pos: FrozenVec
+#     yaw: Yaw
+#     trace: Kind
+#
+#     @property
+#     def orient(self) -> FrozenMatrix:
+#         """Rotate around this yaw."""
+#         return self.yaw.orient
+#
+#     def local(self, x: int, y: int, z: int) -> FrozenVec:
+#         return round(FrozenVec(x, y, z) @ self.yaw.orient + self.pos)
 
 @attrs.frozen
-class CatwalkNode:
-    pos: FrozenVec
-    yaw: Yaw
-    trace: Kind
+class GenericNode:
+    pos: Union[Vec, FrozenVec]
+    ang: Union[Angle, FrozenAngle]
+    trace: NodeKind
 
     @property
     def orient(self) -> FrozenMatrix:
         """Rotate around this yaw."""
-        return self.yaw.orient
+        return FrozenMatrix.from_angle(self.ang)
 
-    def local(self, x: int, y: int, z: int) -> FrozenVec:
-        return round(FrozenVec(x, y, z) @ self.yaw.orient + self.pos)
+    def local(self, p: Union[Vec, FrozenVec]) -> FrozenVec:
+        return round(p @ self.orient + self.pos)
 
 # checked_blocks = []
 # not_void = []
 
-def debug_block(vec : Vec, group : list):
-    if not vec in group:
-        group.append(vec)
+# def debug_block(vec : Vec, group : list):
+#     if not vec in group:
+#         group.append(vec)
 
-def test(vmf: VMF, info: CorrInfo, bounds: Vec.bbox):
-    entry_pos = Vec.from_str(info.inst_entry['origin'])
-    entry_orient = Matrix.from_angstr(info.inst_entry['angles'])
-    start = world_to_grid(Vec(-640, 0, 0) @ entry_orient + entry_pos)
-    yaw = Yaw(round(entry_orient.to_angle().yaw)).right.right
-
+def test(start_node: GenericNode, end_node: GenericNode, node_types: list, info: CorrInfo, bounds: Vec.bbox, blockers: list):
     @functools.cache
     def is_empty(x: int, y: int) -> bool:
         for z in range(26):
@@ -99,7 +107,7 @@ def test(vmf: VMF, info: CorrInfo, bounds: Vec.bbox):
         if Vec(vec.x, vec.y, 0).in_bbox(pos_min, pos_max):
             return True
 
-    def reached_goal(node: CatwalkNode, goal: CatwalkNode) -> bool:
+    def reached_goal(node: GenericNode, goal: GenericNode) -> bool:
         # point = Vec(1, 0, 0) @ node.yaw.orient + node.pos(x, y - 1), (x, y + 1), (x - 1, y), (x + 1, y)]if 0 <= nx < self.width and 0 <= ny < self.height and self.lines[ny][nx] == ' ']
         # debug_block(grid_to_world(point), checked_blocks)
 
@@ -115,18 +123,26 @@ def test(vmf: VMF, info: CorrInfo, bounds: Vec.bbox):
         return node == goal
         # return True
 
-    def neighbours(cur_node: CatwalkNode) -> Iterable[CatwalkNode]:
+    def neighbours(cur_node: GenericNode) -> Iterable[GenericNode]:
+        next_pos = cur_node.trace.end_pos @ cur_node.orient + cur_node.pos
+        if not POS[next_pos] in blockers:
+            for node_type in node_types:
+                for pos in node_type.bbox:
+                    if not POS[pos] in blockers:
+                        yield GenericNode(cur_node.local(node_type.end_pos), cur_node.ang, node_type)
+
+
         # debug_block(grid_to_world(cur_node.pos), checked_blocks)
-        forward = cur_node.local(1, 0, 0)
-        if POS[forward] is Block.VOID and grid_to_world(forward).in_bbox(*bounds):
-            yield CatwalkNode(forward, cur_node.yaw, Kind.STRAIGHT)
-            yield CatwalkNode(forward, cur_node.yaw.left, Kind.CORNER_LEFT)
-            yield CatwalkNode(forward, cur_node.yaw.right, Kind.CORNER_RIGHT)
-            if POS[cur_node.local(2, 0, 0)] is Block.VOID:
-                if POS[cur_node.local(2, 0, 1)] is Block.VOID:
-                    yield CatwalkNode(cur_node.local(2, 0, 1), cur_node.yaw, Kind.STAIR_UP)
-                if POS[cur_node.local(2, 0, -1)] is Block.VOID:
-                    yield CatwalkNode(cur_node.local(2, 0, -1), cur_node.yaw, Kind.STAIR_DN)
+        # forward = cur_node.local(1, 0, 0)
+        # if POS[forward] is Block.VOID and grid_to_world(forward).in_bbox(*bounds):
+        #     yield CatwalkNode(forward, cur_node.yaw, Kind.STRAIGHT)
+        #     yield CatwalkNode(forward, cur_node.yaw.left, Kind.CORNER_LEFT)
+        #     yield CatwalkNode(forward, cur_node.yaw.right, Kind.CORNER_RIGHT)
+        #     if POS[cur_node.local(2, 0, 0)] is Block.VOID:
+        #         if POS[cur_node.local(2, 0, 1)] is Block.VOID:
+        #             yield CatwalkNode(cur_node.local(2, 0, 1), cur_node.yaw, Kind.STAIR_UP)
+        #         if POS[cur_node.local(2, 0, -1)] is Block.VOID:
+        #             yield CatwalkNode(cur_node.local(2, 0, -1), cur_node.yaw, Kind.STAIR_DN)
 
     # def distance(node1: CatwalkNode, node2: CatwalkNode) -> float:
     #     return 8 * (node1.pos - node2.pos).mag()
@@ -139,40 +155,40 @@ def test(vmf: VMF, info: CorrInfo, bounds: Vec.bbox):
     #         return dist * 2
     #     return dist
 
-    def heuristic_cost_estimate(n1: CatwalkNode, n2: CatwalkNode) -> float:
+    def heuristic_cost_estimate(n1: GenericNode, n2: GenericNode) -> float:
         """computes the cost of the path"""
         dist = distance_between(n1, n2)
         cost = dist
-        if n1.trace.is_stair and n2.trace.is_stair and n1.trace is not n2.trace:
-            cost += 40
+        # if n1.trace.is_stair and n2.trace.is_stair and n1.trace is not n2.trace:
+        #     cost += 40
         # keeps making unnecessary stairs
         # elif n1.trace.is_stair and n2.trace == Kind.CORNER_RIGHT or n1.trace == Kind.CORNER_RIGHT and n2.trace.is_stair:
         #     cost += 5
-        elif n2.trace.is_stair:
-            cost += 20
-        elif n2.trace.is_corner:
-            cost += 100
-        if in_inner_bounds(grid_to_world(n2.pos)):
-            cost += cost*2
+        # elif n2.trace.is_stair:
+        #     cost += 20
+        # elif n2.trace.is_corner:
+        #     cost += 100
+        # if in_inner_bounds(grid_to_world(n2.pos)):
+        #     cost += cost*2
         return cost
 
-    def distance_between(n1: CatwalkNode, n2: CatwalkNode) -> float:
+    def distance_between(n1: GenericNode, n2: GenericNode) -> float:
         """computes the distance between neighbors"""
         return 8 * (n1.pos - n2.pos).mag()
 
-    start_node = CatwalkNode(start.freeze(), yaw, Kind.STRAIGHT)
-    end_node = CatwalkNode(FrozenVec(0, 20, start_node.pos.z), Yaw.EAST, Kind.STRAIGHT)
+    # start_node = CatwalkNode(start.freeze(), yaw, Kind.STRAIGHT)
+    # end_node = CatwalkNode(FrozenVec(0, 20, start_node.pos.z), Yaw.EAST, Kind.STRAIGHT)
 
-    vmf.create_ent(
-        'info_particle_system',
-        origin=grid_to_world(start_node.pos.thaw()),
-        angles=start_node.yaw.orient.to_angle(),
-    )
-    vmf.create_ent(
-        'info_particle_system',
-        origin=grid_to_world(end_node.pos.thaw()),
-        angles=end_node.yaw.orient.to_angle(),
-    )
+    # vmf.create_ent(
+    #     'info_particle_system',
+    #     origin=grid_to_world(start_node.pos.thaw()),
+    #     angles=start_node.yaw.orient.to_angle(),
+    # )
+    # vmf.create_ent(
+    #     'info_particle_system',
+    #     origin=grid_to_world(end_node.pos.thaw()),
+    #     angles=end_node.yaw.orient.to_angle(),
+    # )
 
     path = astar.find_path(
         start_node, end_node,
@@ -183,22 +199,23 @@ def test(vmf: VMF, info: CorrInfo, bounds: Vec.bbox):
     )
 
     if path is not None:
-        for node in path:
-            mdl, off, yaw_val = PROPS[node.trace]
-            ang = node.yaw.orient.to_angle()
-            ang.yaw += yaw_val
-            vmf.create_ent(
-                'prop_static',
-                origin=grid_to_world(node.pos.thaw()) + (off @ node.orient),
-                model=mdl,
-                angles=ang,
-            )
-            vmf.create_ent(
-                'info_particle_system',
-                origin=grid_to_world(node.pos.thaw()),
-                angles=node.yaw.orient.to_angle(),
-                targetname = str(node.trace)
-            )
+        return path
+        # for node in path:
+        #     mdl, off, yaw_val = PROPS[node.trace]
+        #     ang = node.yaw.orient.to_angle()
+        #     ang.yaw += yaw_val
+            # vmf.create_ent(
+            #     'prop_static',
+            #     origin=grid_to_world(node.pos.thaw()) + (off @ node.orient),
+            #     model=mdl,
+            #     angles=ang,
+            # )
+            # vmf.create_ent(
+            #     'info_particle_system',
+            #     origin=grid_to_world(node.pos.thaw()),
+            #     angles=node.yaw.orient.to_angle(),
+            #     targetname = str(node.trace)
+            # )
         # for point in checked_blocks:
         #     vmf.create_ent(
         #          'info_target',
