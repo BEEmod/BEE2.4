218f9b8f167a2745c6233c2a8cb113c1abf7ab9f
Spencer Brown <spencerb21@live.com>
2024-03-18 16:41:59 +1000

    Merge test_slicekey into test_plane

--- src/test/test_plane.py
+++ src/test/test_plane.py
@@ -1,17 +1,18 @@
-"""Test the Grid class."""
+"""Test classes in the plane module."""
 from typing import List, Optional, Tuple, no_type_check
 from collections import Counter
 
 import pytest
+from srctools import FrozenVec, Matrix, Vec
 
-from plane import PlaneGrid
+from plane import PlaneGrid, PlaneKey
 
 
 @pytest.mark.parametrize('dx, dy', [
     (-2, 0), (2, 0), (0, -2), (0, 2),
     (2, 2), (-2, 2), (2, -2), (-2, -2),
 ])
-def test_insertion(dx: int, dy: int) -> None:
+def test_grid_insertion(dx: int, dy: int) -> None:
     """Simple tests to ensure resizing works in each direction."""
     grid = PlaneGrid[object]()
     for i in range(10):
@@ -76,7 +77,7 @@ def _points(*pattern: str) -> List[Tuple[int, int]]:
         '54..0',
     ),
 ], ids=['order', 'patA', 'patB', 'patC'])
-def test_insertion_complex(pattern: List[Tuple[int, int]], off_x: int, off_y: int) -> None:
+def test_grid_insertion_complex(pattern: List[Tuple[int, int]], off_x: int, off_y: int) -> None:
     """Insert in various patterns, to test the dynamic resizing."""
     grid = PlaneGrid[object]()
     backup = {}
@@ -107,7 +108,7 @@ def test_insertion_complex(pattern: List[Tuple[int, int]], off_x: int, off_y: in
             assert grid[chk_x, chk_y] == check, backup
 
 
-def test_views() -> None:
+def test_grid_views() -> None:
     """Test the view objects."""
     grid: PlaneGrid[Optional[int]] = PlaneGrid()
     grid[0, 4] = 1
@@ -150,7 +151,7 @@ def test_views() -> None:
 
 # noinspection PyTypeChecker
 @no_type_check
-def test_illegal_positions() -> None:
+def test_grid_illegal_positions() -> None:
     """Test invalid positions produce a KeyError."""
     grid = PlaneGrid()
     grid[1, 2] = 5
@@ -188,7 +189,7 @@ def test_illegal_positions() -> None:
         del grid[9, "blah"]
 
 
-def test_deletion() -> None:
+def test_grid_deletion() -> None:
     """Test deleting positions."""
     grid = PlaneGrid[int]()
     grid[5, 5] = 4
@@ -210,7 +211,7 @@ def test_deletion() -> None:
     assert len(grid) == 1
 
 
-def test_defaults() -> None:
+def test_grid_defaults() -> None:
     """Test the ability to set a default value for all keys."""
     grid_req = PlaneGrid[int]()
     grid_opt = PlaneGrid[int](default=45)
@@ -226,3 +227,76 @@ def test_defaults() -> None:
             with pytest.raises(KeyError):
                 _ = grid_req[x, y]
             assert grid_opt[x, y] == 45
+
+
+def test_key_constructor() -> None:
+    """Test constructing PlaneKey."""
+    key = PlaneKey(Vec(0, 0, 1), 48.0)
+    assert key.normal is Vec.T  # The normal is always these constants.
+    assert key.distance == 48.0
+
+    key = PlaneKey(FrozenVec(-1, 0, 0), 192.84)
+    assert key.normal is Vec.W
+    assert key.distance == 192.84
+
+    key = PlaneKey((0, -1.00000000001, 0.0), (48.0, 26.8, 192.12))
+    assert key.normal is Vec.S
+    assert key.distance == -26.8
+
+    with pytest.raises(ValueError):
+        PlaneKey(Vec(), 12)
+    with pytest.raises(ValueError):
+        PlaneKey(Matrix.from_yaw(45).forward(), 8)
+
+
+def test_key_equality() -> None:
+    """Test equality between PlaneKeys."""
+    key = PlaneKey(Vec.N, 38.0)
+    good = PlaneKey((0, 1, 0), 38)
+    bad1 = PlaneKey((0, 0, -1), 38)
+    bad2 = PlaneKey(Vec.N, -38.0)
+
+    assert key == good
+    assert not (key == bad1)
+    assert not (key == bad2)
+
+    assert not (key != good)
+    assert key != bad1
+    assert key != bad2
+
+    assert hash(key) == hash(good)
+    assert hash(key) != hash(bad1)
+    assert hash(key) != hash(bad2)
+
+
+def test_key_orients() -> None:
+    """Test the key orientations are correct."""
+    # We know Matrix.from_angle() is right, just need to check the correct ones are produced.
+    assert PlaneKey(Vec.N, 12).orient.up() == Vec.N
+    assert PlaneKey(Vec.S, 12).orient.up() == Vec.S
+    assert PlaneKey(Vec.E, 12).orient.up() == Vec.E
+    assert PlaneKey(Vec.W, 12).orient.up() == Vec.W
+    assert PlaneKey(Vec.T, 12).orient.up() == Vec.T
+    assert PlaneKey(Vec.B, 12).orient.up() == Vec.B
+
+    assert PlaneKey(Vec.N, 12).orient.left() == Vec.T
+    assert PlaneKey(Vec.S, 12).orient.left() == Vec.T
+    assert PlaneKey(Vec.E, 12).orient.left() == Vec.T
+    assert PlaneKey(Vec.W, 12).orient.left() == Vec.T
+    assert PlaneKey(Vec.T, 12).orient.left() == Vec.N
+    assert PlaneKey(Vec.B, 12).orient.left() == Vec.N
+
+    assert PlaneKey(Vec.N, 12).orient.forward() == Vec.W
+    assert PlaneKey(Vec.S, 12).orient.forward() == Vec.E
+    assert PlaneKey(Vec.E, 12).orient.forward() == Vec.N
+    assert PlaneKey(Vec.W, 12).orient.forward() == Vec.S
+    assert PlaneKey(Vec.T, 12).orient.forward() == Vec.E
+    assert PlaneKey(Vec.B, 12).orient.forward() == Vec.W
+
+
+@pytest.mark.parametrize('normal', [
+    Vec.N, Vec.S, Vec.E, Vec.W, Vec.T, Vec.B
+])
+def test_key_orient_roundtrip(normal: Vec) -> None:
+    orient = PlaneKey(normal, 123).orient
+    assert orient.to_angle() == Matrix.from_angle(orient.to_angle()).to_angle()
