17683cab7c71918ea81eeab6cc621c1c12557263
Spencer Brown <spencerb21@live.com>
2024-03-20 16:31:26 +1000

    Rearrange dropdown widget logic for new system

--- src/app/itemconfig/dropdown.py
+++ src/app/itemconfig/dropdown.py
@@ -1,12 +1,14 @@
 """A widget for picking from a specific list of options."""
-from typing import Tuple
 from tkinter import ttk
 import tkinter as tk
 
+from trio_util import AsyncValue
 import srctools.logger
+import trio
 
-from packages.widgets import DropdownOptions, UpdateFunc
-from app import itemconfig, localisation
+from packages.widgets import DropdownOptions
+from app import itemconfig
+from transtoken import CURRENT_LANG
 from ui_tk.img import TKImages
 
 
@@ -16,35 +18,45 @@ LOGGER = srctools.logger.get_logger(__name__)
 @itemconfig.ui_single_wconf(DropdownOptions)
 async def dropdown(
     parent: tk.Widget, tk_img: TKImages,
-    on_changed: itemconfig.SingleChangeFunc, conf: DropdownOptions,
-) -> Tuple[tk.Widget, UpdateFunc]:
+    holder: AsyncValue[str],
+    conf: DropdownOptions,
+    /, *, task_status: trio.TaskStatus[tk.Widget] = trio.TASK_STATUS_IGNORED,
+) -> None:
     """Dropdowns allow selecting from a few options."""
-    async def update_ui(new_value: str) -> None:
-        """Update when new values are picked."""
-        try:
-            index = conf.key_to_index[new_value.casefold()]
-        except KeyError:
-            LOGGER.warning('Invalid combobox value: "{}"!', new_value)
-            return
-        combobox.current(index)
-
-    def changed(_: tk.Event) -> None:
-        """Called when the combobox changes."""
-        on_changed(conf.options[combobox.current()])
 
     combobox = ttk.Combobox(
         parent,
         exportselection=False,
         state='readonly',
+        values=[str(token) for token in conf.display],
     )
 
-    @localisation.add_callback(call=True)
-    def update_combo_values() -> None:
-        """Update the combo box when translations change."""
-        index = combobox.current()
-        combobox['values'] = [str(token) for token in conf.display]
-        if index >= 0:  # -1 if empty.
-            combobox.current(index)  # Update display.
+    async def update_ui() -> None:
+        """Update when new values are picked."""
+        async for value in holder.eventual_values():
+            try:
+                index = conf.key_to_index[value.casefold()]
+            except KeyError:
+                LOGGER.warning('Invalid combobox value: "{}"!', value)
+                return
+            combobox.current(index)
 
-    combobox.bind('<<ComboboxSelected>>', changed)
-    return combobox, update_ui
+    async def update_combo_values() -> None:
+        """Update the combo box when translations change."""
+        while True:
+            await CURRENT_LANG.wait_transition()
+            index = combobox.current()
+            combobox['values'] = [str(token) for token in conf.display]
+            if index >= 0:  # -1 if empty.
+                combobox.current(index)  # Update display.
+
+    def changed(_: tk.Event) -> None:
+        """Called when the combobox changes."""
+        holder.value = conf.options[combobox.current()]
+
+    async with trio.open_nursery() as nursery:
+        nursery.start_soon(update_ui)
+        nursery.start_soon(update_combo_values)
+
+        combobox.bind('<<ComboboxSelected>>', changed)
+        task_status.started(combobox)
