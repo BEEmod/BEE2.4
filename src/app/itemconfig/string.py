94142c1b7caebee5ef4b3c99035ad2fbcb1bcad0
Spencer Brown <spencerb21@live.com>
2024-03-20 17:18:17 +1000

    Rearrange text widget for new system

--- src/app/itemconfig/string.py
+++ src/app/itemconfig/string.py
@@ -3,7 +3,10 @@ from __future__ import annotations
 import tkinter as tk
 from tkinter import ttk
 
-from packages.widgets import KIND_STRING, UpdateFunc
+from trio_util import AsyncValue
+import trio
+
+from packages.widgets import KIND_STRING
 from app import itemconfig
 from ui_tk.img import TKImages
 
@@ -11,19 +14,20 @@ from ui_tk.img import TKImages
 @itemconfig.ui_single_no_conf(KIND_STRING)
 async def widget_string(
     parent: tk.Widget, tk_img: TKImages,
-    on_changed: itemconfig.SingleChangeFunc,
-) -> tuple[tk.Widget, UpdateFunc]:
+    holder: AsyncValue[str],
+    /, *, task_status: trio.TaskStatus[tk.Widget] = trio.TASK_STATUS_IGNORED,
+) -> None:
     """Simple textbox for entering text."""
-    var = tk.StringVar()
-    entry = ttk.Entry(
-        parent,
-        textvariable=var,
-    )
-    # When changed, fire the callback.
-    var.trace_add('write', lambda *args: on_changed(var.get()))
+    var = tk.StringVar(parent, holder.value)
+    entry = ttk.Entry(parent, textvariable=var)
 
-    async def update_ui(value: str) -> None:
-        """Called to update the UI."""
+    def on_changed(*args: object) -> None:
+        """When changed, propagate."""
+        holder.value = entry.get()
+
+    var.trace_add('write', on_changed)
+
+    task_status.started(entry)
+
+    async for value in holder.eventual_values():
         var.set(value)
-
-    return entry, update_ui
