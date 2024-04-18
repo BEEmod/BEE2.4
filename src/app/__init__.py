bea95135de5728474ee3cc124c7241750f43d6e6
Spencer Brown <spencerb21@live.com>
2024-04-18 14:03:33 +1000

    Move TK_ROOT to ui_tk package.

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -3,18 +3,13 @@ from __future__ import annotations
 from typing import Any, Awaitable, Callable, TypeVar, Generic, overload
 
 from typing_extensions import TypeVarTuple, Unpack
-from types import TracebackType, new_class
+from types import TracebackType
 import tkinter as tk
 
 from trio_util import AsyncBool
 import trio
 
 import utils
-
-# We must always have one Tk object, and it needs to be constructed
-# before most of TKinter will function. So doing it here does it first.
-TK_ROOT = tk.Tk()
-TK_ROOT.withdraw()  # Hide the window until everything is loaded.
 
 # The nursery where UI tasks etc are run in.
 _APP_NURSERY: trio.Nursery | None = None
@@ -28,21 +23,8 @@ def quit_app() -> None:
     """Quit the application."""
     _APP_QUIT_SCOPE.cancel()
 
-TK_ROOT.protocol("WM_DELETE_WINDOW", quit_app)
 
-if '__class_getitem__' not in vars(tk.Event):
-    # Patch in it being generic, by replacing it with a copy that subclasses Generic.
-    _W_co = TypeVar("_W_co", covariant=True, bound=tk.Misc)
-    _W_co.__module__ = 'tkinter'
-    tk.Event = new_class(  # type: ignore
-        'Event', (Generic[_W_co], ),
-        exec_body=lambda ns: ns.update({
-            name: getattr(tk.Event, name)
-            for name in vars(tk.Event)
-            # Specify the vars to assign, so we don't include things like __dict__ descriptors.
-            if name in ['__doc__', '__module__', '__repr__']
-        }),
-    )
+from ui_tk import TK_ROOT as TK_ROOT  # TODO: Remove this import from here.
 
 
 # noinspection PyBroadException
--- src/ui_tk/__init__.py
+++ src/ui_tk/__init__.py
@@ -1,1 +1,28 @@
 """This package contains UI code specific to TKinter."""
+from typing import Generic, TypeVar
+from types import new_class
+import tkinter as tk
+
+import app
+
+
+# We must always have one Tk object, and it needs to be constructed
+# before most of TKinter will function. So doing it here does it first.
+TK_ROOT = tk.Tk()
+TK_ROOT.withdraw()  # Hide the window until everything is loaded.
+
+TK_ROOT.protocol("WM_DELETE_WINDOW", app.quit_app)
+
+if '__class_getitem__' not in vars(tk.Event):
+    # Patch in it being generic, by replacing it with a copy that subclasses Generic.
+    _W_co = TypeVar("_W_co", covariant=True, bound=tk.Misc)
+    _W_co.__module__ = 'tkinter'
+    tk.Event = new_class(  # type: ignore
+        'Event', (Generic[_W_co], ),
+        exec_body=lambda ns: ns.update({
+            name: getattr(tk.Event, name)
+            for name in vars(tk.Event)
+            # Specify the vars to assign, so we don't include things like __dict__ descriptors.
+            if name in ['__doc__', '__module__', '__repr__']
+        }),
+    )
