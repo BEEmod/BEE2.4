78003c756d6b20ba23a0ccf3dcfb229296c9cb8f
Spencer Brown <spencerb21@live.com>
2024-06-06 11:27:02 +1000

    Use actual random data for test_compile_pane

--- src/test/config/test_compile_pane.py
+++ src/test/config/test_compile_pane.py
@@ -1,20 +1,19 @@
 """Test item default configs."""
-from srctools import Keyvalues
-from srctools.dmx import Element, ValueType, Attribute
-import pytest
+from random import Random
 import base64
+
+
+from srctools import Keyvalues
+from srctools.dmx import Element, Attribute
+import pytest
 
 from config import UnknownVersion
 from config.compile_pane import CompilePaneState
 
 
 # We don't test that the value is actually a screenshot, just use some random data.
-SCREENSHOT_DATA = (
-    b'\x01\x02\x03\x04\x05\x06\x07\x08\t\n\x0b\x0c\r\x0e\x0f\x10\x11\x12\x13\x14\x15\x16'
-    b'\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJK'
-    b'LMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\x7f'
-)
-# Also not testing base64 itself.
+SCREENSHOT_DATA = Random(1234).randbytes(256)
+# Also not testing base64 itself. encodebytes() splits these into 72-long lines.
 SCREENSHOT_BASE64 = base64.encodebytes(SCREENSHOT_DATA).decode('ascii').splitlines()
 
 
