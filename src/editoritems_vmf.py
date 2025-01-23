bc0559f774ac022da767e855f2b5f3929737444d
Spencer Brown <spencerb21@live.com>
2025-01-23 15:15:52 +1000

    Fix antline positioning on items defined by VMF

--- src/editoritems_vmf.py
+++ src/editoritems_vmf.py
@@ -129,8 +129,8 @@ def load_connectionpoint(item: Item, ent: Entity) -> None:
             origin, ent['skin'],
         )
         return
-    ant_pos = Coord(round(center.x + offset.x), round(center.y - offset.y), 0)
-    sign_pos = Coord(round(center.x - offset.x), round(center.y + offset.y), 0)
+    ant_pos = Coord(round(center.x + offset.x), round(center.y + offset.y), 0)
+    sign_pos = Coord(round(center.x - offset.x), round(center.y - offset.y), 0)
 
     group_str = ent['group_id']
 
