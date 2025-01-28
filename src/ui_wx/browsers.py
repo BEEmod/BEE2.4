0baa025cf46036bef7940bd6cfc29399b5eed894
Spencer Brown <spencerb21@live.com>
2025-01-28 13:31:21 +1000

    Use a DataViewModel for the sound browser

--- src/ui_wx/browsers.py
+++ src/ui_wx/browsers.py
@@ -1,16 +1,74 @@
 """Windows to allow browsing for sounds and other files."""
-import wx
+from typing import assert_never, override
+
+from collections.abc import Sequence
+
+from wx.dataview import DataViewCtrl, DataViewItem, DataViewModel
+import wx.dataview
+from srctools import choreo
+from srctools.sndscript import Sound
 
 from app.browsers import SoundBrowserBase
+from app.gameMan import Game
+from packages import PackagesSet
 from ui_wx import MAIN_WINDOW
 
 
-class SoundBrowser(SoundBrowserBase):
+class SoundsModel(DataViewModel):
+    """An interface allowing the control to directly read from the Python list."""
+    def __init__(self, sounds: Sequence[Sound | str | choreo.Entry]) -> None:
+        super().__init__()
+        self.sounds = sounds
 
+    @override
+    def HasDefaultCompare(self) -> bool:
+        return True
+
+    @override
+    def IsContainer(self, item: DataViewItem) -> bool:
+        """We have no hierachy, so no children."""
+        return item.GetID() is None
+
+    @override
+    def GetParent(self, item: DataViewItem) -> DataViewItem:
+        """All items are parents of the root."""
+        return DataViewItem(None)
+
+    @override
+    # Not actually a list, but close enough.
+    def GetChildren(self, item: DataViewItem, children: list[DataViewItem]) -> int:
+        # If None it's the root, so return the number of items.
+        if item.GetID() is not None:
+            # All actual items have no children
+            return 0
+        for i in range(len(self.sounds)):
+            children.append(DataViewItem(i))
+        return len(self.sounds)
+
+    @override
+    def GetValue(self, item: DataViewItem, col: int) -> object:
+        """Get the value to show."""
+        if item.GetID() is None:
+            return ''  # Root
+        pos = int(item.GetID())
+        match self.sounds[pos]:
+            case Sound() as sndscript:
+                return sndscript.name
+            case choreo.Entry() as scene:
+                return scene.filename
+            case str() as raw:
+                return raw
+            case err:
+                assert_never(err)
+
+
+class SoundBrowser(SoundBrowserBase):
     def __init__(self) -> None:
         super().__init__()
 
         self.win = wx.Frame(MAIN_WINDOW)
+        self.view_soundscript = SoundsModel(self._soundscripts)
+        self.view_choreo = SoundsModel(self._scenes)
 
         panel_main = wx.Panel(self.win, wx.ID_ANY)
         sizer_main = wx.BoxSizer(wx.VERTICAL)
@@ -18,7 +76,16 @@ class SoundBrowser(SoundBrowserBase):
         lbl_header = wx.StaticText(panel_main, wx.ID_ANY, "Sounds:")
         sizer_main.Add(lbl_header, 0, wx.LEFT | wx.RIGHT, 7)
 
-        self.wid_soundlist = wx.ListBox(panel_main, wx.ID_ANY, choices=["soundb", "sounda"], style=wx.LB_ALWAYS_SB | wx.LB_SINGLE | wx.LB_SORT)
+        # self.wid_soundlist = wx.ListBox(panel_main, wx.ID_ANY, choices=["soundb", "sounda"], style=wx.LB_ALWAYS_SB | wx.LB_SINGLE | wx.LB_SORT)
+        self.wid_soundlist = DataViewCtrl(
+            panel_main,
+            style=wx.dataview.DV_SINGLE | wx.dataview.DV_NO_HEADER | wx.dataview.DV_HORIZ_RULES,
+        )
+        self.soundlist_col = self.wid_soundlist.AppendTextColumn(
+            label='Sounds',
+            model_column=0,
+        )
+        self.wid_soundlist.AssociateModel(self.view_soundscript)
         sizer_main.Add(self.wid_soundlist, 1, wx.BOTTOM | wx.EXPAND | wx.LEFT | wx.RIGHT, 8)
 
         sizer_info = wx.FlexGridSizer(4, 2, 0, 0)
@@ -57,8 +124,8 @@ class SoundBrowser(SoundBrowserBase):
         sizer_checks.Add(self.wid_chk_autoplay, 0, wx.ALL, 4)
 
         self.wid_chk_raw = wx.CheckBox(panel_main, wx.ID_ANY, "Raw sounds")
-        self.wid_chk_raw.Value = True
         sizer_checks.Add(self.wid_chk_raw, 0, wx.ALL, 4)
+        self.wid_chk_raw.Bind(wx.EVT_CHECKBOX, self._evt_change_raw)
 
         sizer_btn = wx.BoxSizer(wx.HORIZONTAL)
         sizer_main.Add(sizer_btn, 0, wx.ALIGN_CENTER_HORIZONTAL | wx.BOTTOM, 8)
@@ -83,3 +150,12 @@ class SoundBrowser(SoundBrowserBase):
 
     def _ui_hide_window(self) -> None:
         self.win.Hide()
+
+    def _evt_change_raw(self, event: wx.Event) -> None:
+        self.wid_soundlist.AssociateModel(self.view_choreo if self.wid_chk_raw.IsChecked() else self.view_soundscript)
+
+    async def _reload(self, packset: PackagesSet, game: Game) -> None:
+        await super()._reload(packset, game)
+        # Indicate both have been completely changed.
+        self.view_soundscript.Cleared()
+        self.view_choreo.Cleared()
