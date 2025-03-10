bc5d372fffedfec10d632c33442a7f6b12cf846c
Spencer Brown <spencerb21@live.com>
2025-03-10 15:38:04 +1000

    Test AntTex.parse()

---
+++ src/test/precomp/test_antlines.py
@@ -0,0 +1,32 @@
+"""Test antline configs."""
+
+import pytest
+from srctools import Keyvalues
+
+# Ensure correct import order. TODO fix cycle
+import vbsp  # noqa
+from precomp.antlines import AntTex
+
+
+def test_parse_ant_tex() -> None:
+    """Test parsing antline texture definitions."""
+    with pytest.raises(IndexError, match=r'No key "?tex"?'):
+        # Material is required.
+        AntTex.parse(Keyvalues('antline', [
+            Keyvalues('scale', '0.25'),
+            Keyvalues('static', '0'),
+        ]))
+    assert AntTex.parse(Keyvalues('antline', [
+        Keyvalues('scale', '.375'),
+        Keyvalues('static', '1'),
+        Keyvalues('tex', 'anim_wp/framework/squarebeams'),
+    ])) == AntTex('anim_wp/framework/squarebeams', 0.375, True)
+    assert AntTex.parse(
+        Keyvalues('ant', 'signage/INDICATOR/indicator_straight')
+    ) == AntTex('signage/INDICATOR/indicator_straight', 0.25, False)
+    assert AntTex.parse(
+        Keyvalues('ant', '-1|signage/INDICATOR/indicator_straight')
+    ) == AntTex('signage/INDICATOR/indicator_straight', -1.0, False)
+    assert AntTex.parse(
+        Keyvalues('ant', '0.5|signage/another|static')
+    ) == AntTex('signage/another', 0.5, True)
